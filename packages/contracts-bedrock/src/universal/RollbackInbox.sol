f765427c5c8e250f2203851a1a3b6fcfd18693b0
0xng <ng@defi.sucks>
2024-05-10 13:11:15 -0300

    chore: adding RollbackInbox contract

---
+++ packages/contracts-bedrock/src/universal/RollbackInbox.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+contract RollbackInbox {
+    // Note: this is not handling initialization of these variables
+    address public otherMessenger; // CrossDomainMessenger from the other domain
+    CrossDomainMessenger public messenger; // CrossDomainMessenger from this domain
+
+    mapping(bytes32 => uint256) messageHashes;
+
+    event MessageHashReceived(bytes32 messageHash, uint256 timestamp);
+
+    function receiveMessageHash(bytes32 _messageHash, address _sender) public {
+        require(
+            messenger.xDomainMsgSender == address(otherMessenger),
+            "RollbackInbox: only CrossDomainMessenger from other domain can be the sender"
+        );
+        require(
+            msg.sender == address(messenger),
+            "RollbackInbox: only CrossDomainMessenger from this domain can be the caller"
+        );
+        require(
+            messenger.successfulMessages[_messageHash] == 0,
+            "RollbackInbox: can't process already successful message hashes"
+        );
+
+        messageHashes[_messageHash] = block.timestamp;
+        emit MessageHashReceived(messageHash, block.timestamp);
+    }
+}
