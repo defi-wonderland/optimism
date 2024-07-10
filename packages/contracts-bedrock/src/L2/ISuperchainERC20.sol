24ec67c06269e1f11aac669c94cec809f74d3002
agusduha <agusnduha@gmail.com>
2024-07-10 16:32:46 -0300

    feat: add mint and burn funcs to superchain erc20

--- packages/contracts-bedrock/src/L2/ISuperchainERC20.sol
+++ packages/contracts-bedrock/src/L2/ISuperchainERC20.sol
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
-import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-
 /// @title ISuperchainERC20
 /// @notice This interface is available on the SuperchainERC20 contract.
 ///         We declare it as a separate interface so that it can be used in
 ///         custom implementations of SuperchainERC20.
-interface ISuperchainERC20 is IERC165 {
-    /// @notice Address of the L2ToL2CrossDomainMessenger.
-    function messenger() external view returns (address);
+interface ISuperchainERC20 {
+    /// @notice Address of the StandardBridge on this network.
+    function bridge() external view returns (address);
+
+    /// @notice Allows the StandardBridge on this network to mint tokens.
+    /// @param _to     Address to mint tokens to.
+    /// @param _amount Amount of tokens to mint.
+    function mint(address _to, uint256 _amount) external;
+
+    /// @notice Allows the StandardBridge on this network to burn tokens.
+    /// @param _from   Address to burn tokens from.
+    /// @param _amount Amount of tokens to burn.
+    function burn(address _from, uint256 _amount) external;
 
     /// @notice Sends tokens to some target address on another chain.
     /// @param _to      Address to send tokens to.
--- packages/contracts-bedrock/src/L2/SuperchainERC20.sol
+++ packages/contracts-bedrock/src/L2/SuperchainERC20.sol
@@ -2,11 +2,11 @@
 pragma solidity 0.8.25;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import { ISuperchainERC20 } from "src/L2/ISuperchainERC20.sol";
 import { IL2ToL2CrossDomainMessenger } from "src/L2/IL2ToL2CrossDomainMessenger.sol";
 import { ISemver } from "src/universal/ISemver.sol";
 import { SafeCall } from "src/libraries/SafeCall.sol";
+import { Predeploys } from "src/libraries/Predeploys.sol";
 
 /// @notice Thrown when attempting to relay a message and the function caller (msg.sender) is not
 /// L2ToL2CrossDomainMessenger.
@@ -18,6 +18,9 @@ error MessageSenderNotThisSuperchainERC20();
 /// @notice Thrown when while relaying tokens the external call fails.
 error ExternalCallFailed();
 
+/// @notice Thrown when attempting to mint or burn tokens and the function caller is not the StandardBridge.
+error CallerNotBridge();
+
 /// @custom:proxied
 /// @title SuperchainERC20
 /// @notice SuperchainERC20 is a standard extension of the base ERC20 token contract that unifies ERC20 token bridging
@@ -26,10 +29,23 @@ error ExternalCallFailed();
 ///         replay protection and domain binding.
 contract SuperchainERC20 is ISuperchainERC20, ERC20, ISemver {
     /// @notice Address of the L2ToL2CrossDomainMessenger.
-    address private immutable MESSENGER;
+    address internal constant MESSENGER = Predeploys.L2_TO_L2_CROSS_DOMAIN_MESSENGER;
+
+    /// @notice Address of the StandardBridge on this network.
+    address private immutable BRIDGE;
 
     /// @notice Decimals of the token
     uint8 private immutable DECIMALS;
+
+    /// @notice Emitted whenever tokens are minted for an account.
+    /// @param account Address of the account tokens are being minted for.
+    /// @param amount  Amount of tokens minted.
+    event Mint(address indexed account, uint256 amount);
+
+    /// @notice Emitted whenever tokens are burned from an account.
+    /// @param account Address of the account tokens are being burned from.
+    /// @param amount  Amount of tokens burned.
+    event Burn(address indexed account, uint256 amount);
 
     /// @notice Emitted whenever tokens are sent to another chain.
     /// @param from    Address of the sender.
@@ -45,24 +61,39 @@ contract SuperchainERC20 is ISuperchainERC20, ERC20, ISemver {
     /// @param data   Data sent with the message.
     event RelayedERC20(address indexed to, uint256 amount, bytes data);
 
+    /// @notice A modifier that only allows the bridge to call
+    modifier onlyBridge() {
+        if (msg.sender != BRIDGE) revert CallerNotBridge();
+        _;
+    }
+
     /// @notice Semantic version.
     /// @custom:semver 1.0.0
     string public constant version = "1.0.0";
 
-    /// @param _messenger   Address of the L2ToL2CrossDomainMessenger.
+    /// @param _bridge      Address of the L2 standard bridge.
     /// @param _name        ERC20 name.
     /// @param _symbol      ERC20 symbol.
     /// @param _decimals    ERC20 decimals.
-    constructor(
-        address _messenger,
-        string memory _name,
-        string memory _symbol,
-        uint8 _decimals
-    )
-        ERC20(_name, _symbol)
-    {
-        MESSENGER = _messenger;
+    constructor(address _bridge, string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol) {
+        BRIDGE = _bridge;
         DECIMALS = _decimals;
+    }
+
+    /// @notice Allows the StandardBridge on this network to mint tokens.
+    /// @param _to     Address to mint tokens to.
+    /// @param _amount Amount of tokens to mint.
+    function mint(address _to, uint256 _amount) external virtual override onlyBridge {
+        _mint(_to, _amount);
+        emit Mint(_to, _amount);
+    }
+
+    /// @notice Allows the StandardBridge on this network to burn tokens.
+    /// @param _from   Address to burn tokens from.
+    /// @param _amount Amount of tokens to burn.
+    function burn(address _from, uint256 _amount) external virtual override onlyBridge {
+        _burn(_from, _amount);
+        emit Burn(_from, _amount);
     }
 
     /// @notice Sends tokens to some target address on another chain.
@@ -100,18 +131,9 @@ contract SuperchainERC20 is ISuperchainERC20, ERC20, ISemver {
         emit RelayedERC20(_to, _amount, _data);
     }
 
-    /// @notice ERC165 interface check function.
-    /// @param _interfaceId Interface ID to check.
-    /// @return Whether or not the interface is supported by this contract.
-    function supportsInterface(bytes4 _interfaceId) external pure virtual returns (bool) {
-        bytes4 iface1 = type(IERC165).interfaceId;
-        bytes4 iface2 = type(ISuperchainERC20).interfaceId;
-        return _interfaceId == iface1 || _interfaceId == iface2;
-    }
-
-    /// @notice Returns the address of the L2ToL2CrossDomainMessenger.
-    function messenger() external view override returns (address) {
-        return MESSENGER;
+    /// @notice Returns the address of the StandardBridge.
+    function bridge() public view override returns (address) {
+        return BRIDGE;
     }
 
     /// @notice Returns the number of decimals used to get its user representation.
