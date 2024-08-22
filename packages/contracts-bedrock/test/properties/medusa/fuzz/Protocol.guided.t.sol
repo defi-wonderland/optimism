b35b4378a343d3b00863aba2e7839d241e91bbb7
teddy <teddy@defi.sucks>
2024-08-22 20:58:26 -0300

    chore: rename bridge->senderc20 method for consistency with relayerc20

--- packages/contracts-bedrock/test/properties/medusa/fuzz/Protocol.guided.t.sol
+++ packages/contracts-bedrock/test/properties/medusa/fuzz/Protocol.guided.t.sol
@@ -88,7 +88,7 @@ contract ProtocolGuided is ProtocolHandler {
     /// @custom:property sendERC20 decreases sender balance in source chain exactly by the input amount
     /// @custom:property-id 10
     /// @custom:property sendERC20 decreases total supply in source chain exactly by the input amount
-    function fuzz_BridgeSupertoken(
+    function fuzz_SendERC20(
         uint256 fromIndex,
         uint256 recipientIndex,
         uint256 destinationChainId,
@@ -157,7 +157,7 @@ contract ProtocolGuided is ProtocolHandler {
 
     /// @custom:property-id 8
     /// @custom:property calls to sendERC20 with a value of zero dont modify accounting
-    // @notice is a subset of fuzz_BridgeSupertoken, so we'll just call it
+    // @notice is a subset of fuzz_SendERC20, so we'll just call it
     // instead of re-implementing it. Keeping the function for visibility of the property.
     function fuzz_SendZeroDoesNotModifyAccounting(
         uint256 fromIndex,
@@ -166,7 +166,7 @@ contract ProtocolGuided is ProtocolHandler {
     )
         external
     {
-        fuzz_BridgeSupertoken(fromIndex, recipientIndex, destinationChainId, 0);
+        fuzz_SendERC20(fromIndex, recipientIndex, destinationChainId, 0);
     }
 
     /// @custom:property-id 9
