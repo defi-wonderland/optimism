e97fda0712654f4b0d5d4c2557af8def04c1b8d8
teddy <teddy@defi.sucks>
2024-08-29 13:45:59 -0300

    chore: move the properties file to its own directory

--- packages/contracts-bedrock/test/properties/medusa/properties/Protocol.t.sol
+++ packages/contracts-bedrock/test/properties/medusa/properties/Protocol.t.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.25;
 
 import { ITokenMock } from "@crytic/properties/contracts/ERC20/external/util/ITokenMock.sol";
 import { EnumerableMap } from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
-import { ProtocolGuided } from "./fuzz/Protocol.guided.t.sol";
-import { ProtocolUnguided } from "./fuzz/Protocol.unguided.t.sol";
+import { ProtocolGuided } from "../fuzz/Protocol.guided.t.sol";
+import { ProtocolUnguided } from "../fuzz/Protocol.unguided.t.sol";
 import { CryticERC20ExternalBasicProperties } from
     "@crytic/properties/contracts/ERC20/external/properties/ERC20ExternalBasicProperties.sol";
 import { OptimismSuperchainERC20 } from "src/L2/OptimismSuperchainERC20.sol";
