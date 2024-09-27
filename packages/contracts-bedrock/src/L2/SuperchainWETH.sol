71fb78cbc2328bf2576d4d0af5d1f4567e225e9a
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2024-09-27 18:19:50 -0300

    chore: add natspec on the isuperchainweth

--- packages/contracts-bedrock/src/L2/SuperchainWETH.sol
+++ packages/contracts-bedrock/src/L2/SuperchainWETH.sol
@@ -5,7 +5,6 @@ pragma solidity 0.8.15;
 import { WETH98 } from "src/universal/WETH98.sol";
 
 // Libraries
-import { NotCustomGasToken } from "src/libraries/errors/CommonErrors.sol";
 import { Predeploys } from "src/libraries/Predeploys.sol";
 
 // Interfaces
