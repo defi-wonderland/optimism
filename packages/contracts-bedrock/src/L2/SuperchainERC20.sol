e3f474896bb2c0123fe27e0c889fbc54c1653c1f
0xng <ng@defi.sucks>
2024-10-09 12:05:34 -0300

    feat: example integration

--- packages/contracts-bedrock/src/L2/SuperchainERC20.sol
+++ packages/contracts-bedrock/src/L2/SuperchainERC20.sol
@@ -43,4 +43,41 @@ abstract contract SuperchainERC20 is ERC20, ICrosschainERC20, ISemver {
 
         emit CrosschainBurnt(_from, _amount);
     }
+
+    function handleExpireMessage(
+        uint256 destination,
+        uint256 nonce,
+        address sender,
+        address target,
+        bytes memory message,
+        address to,
+        uint256 amount
+    )
+        external
+    {
+        require(sender == Predeploys.SUPERCHAIN_ERC20_BRIDGE);
+        require(target == address(this));
+
+        // this forces the msg.sender to be the one that sent the message in bridge, could also be permissionless if
+        // added as parameter
+        bytes memory expectedMessage =
+            abi.encodeCall(Predeploys.SUPERCHAIN_ERC20_BRIDGE.relayERC20, (address(this), msg.sender, to, amount));
+        require(expectedMessage = message);
+
+        bytes32 messageHash = Hashing.hashL2toL2CrossDomainMessage({
+            _destination: destination,
+            _source: block.chainId,
+            _nonce: nonce,
+            _sender: sender,
+            _target: target,
+            _message: message
+        });
+
+        bool isExpired = Predeploys.L2ToL2CrossDomainMessenger.expiredMessages(msgHash) != 0;
+
+        if (isExpired) {
+            // a recovery address could be added to the message as well
+            _mint(msg.sender, amount);
+        }
+    }
 }
