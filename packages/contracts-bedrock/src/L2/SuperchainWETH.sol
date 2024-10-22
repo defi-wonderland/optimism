b30ad5fd03420d6372f6beb6c2b569db57afb470
agusduha <agusnduha@gmail.com>
2024-10-22 10:03:41 -0300

    fix: semver

--- packages/contracts-bedrock/src/L2/SuperchainWETH.sol
+++ packages/contracts-bedrock/src/L2/SuperchainWETH.sol
@@ -22,8 +22,8 @@ import { Unauthorized, NotCustomGasToken } from "src/libraries/errors/CommonErro
 ///         do not use a custom gas token.
 contract SuperchainWETH is WETH98, ICrosschainERC20, ISemver {
     /// @notice Semantic version.
-    /// @custom:semver 1.0.0-beta.7
-    string public constant version = "1.0.0-beta.7";
+    /// @custom:semver 1.0.0-beta.8
+    string public constant version = "1.0.0-beta.8";
 
     /// @inheritdoc WETH98
     function deposit() public payable override {
