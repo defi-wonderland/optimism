9beed5d6508220af626380fb0407e4c3ebd9cefd
Flux <iamflux@defi.sucks>
2024-11-20 09:13:33 -0500

    refactor: remove context from Entrypoint

--- packages/contracts-bedrock/src/L2/interfaces/IEntrypoint.sol
+++ packages/contracts-bedrock/src/L2/interfaces/IEntrypoint.sol
@@ -7,7 +7,6 @@ interface IEntrypoint {
     /// @notice Executes logic in the entrypoint contract before relaying the message.
     ///         This method is called by the L2ToL2CrossDomainMessenger before relaying the message and passing the
     ///         context that was used in the origin chain.
-    /// @param _context The context data for the relay message, passed as bytes.
     /// @return success_ A boolean indicating whether the operation was successful.
-    function onRelayMessage(bytes calldata _context) external returns (bool success_);
+    function onRelayMessage() external returns (bool success_);
 }
