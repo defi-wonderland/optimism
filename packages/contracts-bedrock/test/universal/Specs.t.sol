2d28252697fb193048ffa61bbf4f9b334f477f09
agusduha <agusnduha@gmail.com>
2024-11-27 14:41:29 -0300

    fix: remove OPCM interop

--- packages/contracts-bedrock/test/universal/Specs.t.sol
+++ packages/contracts-bedrock/test/universal/Specs.t.sol
@@ -856,18 +856,6 @@ contract Specification_Test is CommonTest {
         _addSpec({ _name: "OPContractsManager", _sel: OPContractsManager.chainIdToBatchInboxAddress.selector });
         _addSpec({ _name: "OPContractsManager", _sel: OPContractsManager.implementations.selector });
 
-        // OPContractsManagerInterop
-        _addSpec({ _name: "OPContractsManagerInterop", _sel: _getSel("version()") });
-        _addSpec({ _name: "OPContractsManagerInterop", _sel: _getSel("superchainConfig()") });
-        _addSpec({ _name: "OPContractsManagerInterop", _sel: _getSel("protocolVersions()") });
-        _addSpec({ _name: "OPContractsManagerInterop", _sel: _getSel("l1ContractsRelease()") });
-        _addSpec({ _name: "OPContractsManagerInterop", _sel: _getSel("systemConfigs(uint256)") });
-        _addSpec({ _name: "OPContractsManagerInterop", _sel: _getSel("OUTPUT_VERSION()") });
-        _addSpec({ _name: "OPContractsManagerInterop", _sel: OPContractsManager.deploy.selector });
-        _addSpec({ _name: "OPContractsManagerInterop", _sel: OPContractsManager.blueprints.selector });
-        _addSpec({ _name: "OPContractsManagerInterop", _sel: OPContractsManager.chainIdToBatchInboxAddress.selector });
-        _addSpec({ _name: "OPContractsManagerInterop", _sel: OPContractsManager.implementations.selector });
-
         // DeputyGuardianModule
         _addSpec({
             _name: "DeputyGuardianModule",
