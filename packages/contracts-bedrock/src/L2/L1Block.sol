b7664c3e34f615debacfd643e35abafc044abdaf
Flux <iamflux@defi.sucks>
2025-01-02 14:31:19 -0500

    refactor: add internal _setGasPayingToken to avoid code duplication

--- packages/contracts-bedrock/src/L2/L1Block.sol
+++ packages/contracts-bedrock/src/L2/L1Block.sol
@@ -190,9 +190,7 @@ contract L1Block is ISemver, IGasToken {
     function setGasPayingToken(address _token, uint8 _decimals, bytes32 _name, bytes32 _symbol) external {
         if (msg.sender != DEPOSITOR_ACCOUNT()) revert NotDepositor();
 
-        GasPayingToken.set({ _token: _token, _decimals: _decimals, _name: _name, _symbol: _symbol });
-
-        emit GasPayingTokenSet({ token: _token, decimals: _decimals, name: _name, symbol: _symbol });
+        _setGasPayingToken(_token, _decimals, _name, _symbol);
     }
 
     /// @notice Sets static configuration options for the L2 system. Can only be called by the special
@@ -236,8 +234,17 @@ contract L1Block is ISemver, IGasToken {
     ///               The expected order is: [token address, decimals, name, symbol].
     function _setGasPayingToken(bytes memory _value) internal {
         (address token, uint8 decimals, bytes32 name, bytes32 symbol) = StaticConfig.decodeSetGasPayingToken(_value);
-        GasPayingToken.set({ _token: token, _decimals: decimals, _name: name, _symbol: symbol });
+        _setGasPayingToken(token, decimals, name, symbol);
+    }
 
-        emit GasPayingTokenSet({ token: token, decimals: decimals, name: name, symbol: symbol });
+    /// @notice Internal method to set the gas paying token.
+    /// @param _token    The address of the gas paying token.
+    /// @param _decimals The number of decimals for the gas paying token.
+    /// @param _name     The name of the gas paying token.
+    /// @param _symbol   The symbol of the gas paying token.
+    function _setGasPayingToken(address _token, uint8 _decimals, bytes32 _name, bytes32 _symbol) internal {
+        GasPayingToken.set({ _token: _token, _decimals: _decimals, _name: _name, _symbol: _symbol });
+
+        emit GasPayingTokenSet({ token: _token, decimals: _decimals, name: _name, symbol: _symbol });
     }
 }
