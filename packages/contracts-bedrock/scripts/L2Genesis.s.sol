dee064465138a53518ab175d1effd293c196afe2
Flux <iamflux@defi.sucks>
2025-01-02 12:24:28 -0500

    refactor: remove unused imports in L2Genesis script

--- packages/contracts-bedrock/scripts/L2Genesis.s.sol
+++ packages/contracts-bedrock/scripts/L2Genesis.s.sol
@@ -17,9 +17,6 @@ import { Predeploys } from "src/libraries/Predeploys.sol";
 import { Preinstalls } from "src/libraries/Preinstalls.sol";
 
 // Interfaces
-import { ISequencerFeeVault } from "interfaces/L2/ISequencerFeeVault.sol";
-import { IBaseFeeVault } from "interfaces/L2/IBaseFeeVault.sol";
-import { IL1FeeVault } from "interfaces/L2/IL1FeeVault.sol";
 import { IOptimismMintableERC721Factory } from "interfaces/L2/IOptimismMintableERC721Factory.sol";
 import { IGovernanceToken } from "interfaces/governance/IGovernanceToken.sol";
 import { IOptimismMintableERC20Factory } from "interfaces/universal/IOptimismMintableERC20Factory.sol";
