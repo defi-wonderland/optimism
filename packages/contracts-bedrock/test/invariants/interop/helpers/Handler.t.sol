28d258ae6c09d2b339fa20cc724e171d83722690
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-01-09 17:25:02 -0300

    fix: use msg hash

--- packages/contracts-bedrock/test/invariants/interop/helpers/Handler.t.sol
+++ packages/contracts-bedrock/test/invariants/interop/helpers/Handler.t.sol
@@ -5,6 +5,7 @@ import { Setup } from "../Setup.sol";
 import { Actors } from "./Actors.t.sol";
 import { Identifier } from "interfaces/L2/ICrossL2Inbox.sol";
 import { Utils } from "../utils/Utils.sol";
+import { Hashing } from "src/libraries/Hashing.sol";
 
 import "forge-std/Test.sol";
 
@@ -96,7 +97,15 @@ contract Handler is Setup {
         );
 
         // Ensure the message is not already relayed
-        require(!L2_TO_L2_MESSENGER.successfulMessages(_id, sentMessage));
+        bytes32 messageHash = Hashing.hashL2toL2CrossDomainMessage({
+            _destination: block.chainid,
+            _source: _id.chainId,
+            _nonce: _message.nonce,
+            _sender: address(SUPER_WETH),
+            _target: address(SUPER_WETH),
+            _message: message
+        });
+        require(!L2_TO_L2_MESSENGER.successfulMessages(messageHash));
 
         Actors actor = randomActor(_toActorIndex);
         try actor.callL2ToL2MessengerRelayMessage(_id, sentMessage) {
