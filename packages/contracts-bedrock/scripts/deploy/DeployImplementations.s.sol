4fdb9bb0a973866795f49631178e8411b89be910
Flux <iamflux@defi.sucks>
2025-01-21 16:42:11 -0500

    chore: update label

--- packages/contracts-bedrock/scripts/deploy/DeployImplementations.s.sol
+++ packages/contracts-bedrock/scripts/deploy/DeployImplementations.s.sol
@@ -653,7 +653,7 @@ contract DeployImplementations is Script {
             );
         }
 
-        vm.label(address(impl), "OptimismMintableERC20FactoryImpl");
+        vm.label(address(impl), "L1OptimismMintableERC20FactoryImpl");
         _dio.set(_dio.optimismMintableERC20FactoryImpl.selector, address(impl));
     }
 
