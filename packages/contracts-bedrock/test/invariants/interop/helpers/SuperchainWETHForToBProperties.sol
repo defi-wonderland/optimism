567c7ad7e20d0bfe75db6deea0cc4670a7682615
hexshire <hexshire@defi.sucks>
2025-01-22 18:33:15 -0300

    chore: setup token variable

--- packages/contracts-bedrock/test/invariants/interop/helpers/SuperchainWETHForToBProperties.sol
+++ packages/contracts-bedrock/test/invariants/interop/helpers/SuperchainWETHForToBProperties.sol
@@ -1,24 +1,24 @@
 // SPDX-License-Identifier: AGPL-3
 pragma solidity ^0.8.0;
 
+import { vm } from "../utils/VM.sol";
 import { SuperchainWETH } from "src/L2/SuperchainWETH.sol";
 import "properties/contracts/ERC20/external/properties/ERC20ExternalBasicProperties.sol";
 import "properties/contracts/ERC20/external/properties/ERC20ExternalIncreaseAllowanceProperties.sol";
-import "properties/contracts/ERC20/external/properties/ERC20ExternalMintableProperties.sol";
-import "properties/contracts/ERC20/external/properties/ERC20ExternalBurnableProperties.sol";
 
 contract SuperchainWETHForToBProperties is
     SuperchainWETH,
     CryticERC20ExternalBasicProperties,
-    CryticERC20ExternalIncreaseAllowanceProperties,
-    CryticERC20ExternalMintableProperties,
-    CryticERC20ExternalBurnableProperties
+    CryticERC20ExternalIncreaseAllowanceProperties
 {
     /// @notice This is used by CryticERC20ExternalBasicProperties to check the ERC20 properties
     bool public isMintableOrBurnable;
     uint256 public initialSupply;
 
     constructor() {
+        token = ITokenMock(address(this));
+
+        vm.deal(address(this), INITIAL_BALANCE * 4);
         _mint(USER1, INITIAL_BALANCE);
         _mint(USER2, INITIAL_BALANCE);
         _mint(USER3, INITIAL_BALANCE);
@@ -26,13 +26,5 @@ contract SuperchainWETHForToBProperties is
 
         isMintableOrBurnable = true;
         initialSupply = address(this).balance;
-    }
-
-    function mint(address _to, uint256 _amount) public {
-        _mint(_to, _amount);
-    }
-
-    function burn(uint256 _amount) public {
-        _burn(msg.sender, _amount);
     }
 }
