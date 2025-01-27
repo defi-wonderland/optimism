fb4bb5d2715073785247672af6cfb04a0714d7a1
hexshire <hexshire@defi.sucks>
2025-01-27 13:58:25 -0300

    chore: remove modifier assert false

--- packages/contracts-bedrock/test/invariants/interop/helpers/Handler.sol
+++ packages/contracts-bedrock/test/invariants/interop/helpers/Handler.sol
@@ -38,8 +38,6 @@ contract Handler is Setup {
             initialized = true;
         }
         _;
-        // TODO: Remove this
-        assert(false);
     }
 
     function handler_transferSuperchainERC20(address _to, uint256 _amount, uint256 _actorIndex) public isInitialized {
