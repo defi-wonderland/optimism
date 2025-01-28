f7aece26d6292877514c57fb1ff2c6aafd43a59c
hexshire <hexshire@defi.sucks>
2025-01-28 14:18:44 -0300

    chore: remove test event

--- packages/contracts-bedrock/test/invariants/interop/mocks/OptimismPortalMock.sol
+++ packages/contracts-bedrock/test/invariants/interop/mocks/OptimismPortalMock.sol
@@ -678,10 +678,8 @@ contract OptimismPortalInteropMock is OptimismPortal2Mock {
         ISharedLockbox(s.sharedLockbox).lockETH{ value: msg.value }();
     }
 
-    event Touched(address, address);
     /// @notice Migrates the ETH liquidity to the SharedLockbox. This function will only be called once by the
     ///         SuperchainConfig when adding this chain to the dependency set.
-
     function migrateLiquidity() external {
         if (msg.sender != address(superchainConfig)) revert Unauthorized();
 
@@ -689,7 +687,7 @@ contract OptimismPortalInteropMock is OptimismPortal2Mock {
         s.migrated = true;
 
         uint256 ethBalance = address(this).balance;
-        emit Touched(s.sharedLockbox, address(0));
+
         ISharedLockbox(s.sharedLockbox).lockETH{ value: ethBalance }();
 
         emit ETHMigrated(ethBalance);
