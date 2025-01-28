02564cc89765eef6bcb2d3195a3295c916a40e4f
hexshire <hexshire@defi.sucks>
2025-01-28 17:52:02 -0300

    test: deposit tx

--- packages/contracts-bedrock/test/invariants/interop/FuzzTest.sol
+++ packages/contracts-bedrock/test/invariants/interop/FuzzTest.sol
@@ -294,4 +294,54 @@ contract FuzzTest is Handler {
         // The user balances sum should be equal to the total supply less the Ether relayed or sent to SuperWETH
         assert(_ghost_superWethBalancesSum == SUPER_WETH.totalSupply() - _ghost_superWethEtherSent);
     }
+
+    /// @custom:property-id 10
+    /// @custom:property Before migration, deposits with value greater than zero MUST keep the ETH in the OptimismPortal
+    /// @custom:property-id 12
+    /// @custom:property After migration, the OptimismPortal MUST lock the ETH amount on the SharedLockbox when on a
+    /// deposit transaction with value greater than zero, without holding any ETH balance from the depositing users
+    function test_optimismPortalDeposits(
+        address _to,
+        uint256 _value,
+        uint64 _gasLimit,
+        bool _isCreation,
+        //bytes memory _data,
+        uint256 _actorIndex
+    )
+        public
+        initialize
+    {
+        Actors actor = randomActor(_actorIndex);
+
+        _value = clampLte(_value, address(actor).balance);
+
+        (bool success, bytes memory returnData) = actor.directCall(
+            address(PORTAL),
+            _value,
+            abi.encodeCall(PORTAL.depositTransaction, (_to, _value, _gasLimit, _isCreation, bytes("")))
+        );
+
+        uint256 balanceBefore = _ghost_isMigrated ? address(SHARED_LOCKBOX).balance : address(PORTAL).balance;
+
+        if (success) {
+            if (_ghost_isMigrated) {
+                assert(address(SHARED_LOCKBOX).balance == balanceBefore + _value);
+            } else {
+                assert(address(PORTAL).balance == balanceBefore + _value);
+            }
+        } else {
+            assert(
+                bytes4(returnData) == bytes4(0x77ebef4d) // OutOfGas()
+                    || bytes4(returnData) == bytes4(0x4929b808) // SmallGasLimit()
+                    || bytes4(returnData) == bytes4(0x13496fda) // BadTarget()
+            );
+        }
+    }
+
+    /// @custom:property-id 11
+    /// @custom:property Before migration, withdrawals MUST use the OptimismPortal’s own ETH balance
+    /// @custom:property-id 13
+    /// @custom:property After migration, the OptimismPortal MUST unlock the ETH amount being withdrawn from the
+    /// SharedLockbox if it is greater than zero
+    function test_optimismPortalWithdrawals() public initialize { }
 }
