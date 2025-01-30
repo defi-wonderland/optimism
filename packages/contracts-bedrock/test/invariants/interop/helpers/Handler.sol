015f900ca03600e976a60d1f71bcc00ed128cb7c
hexshire <hexshire@defi.sucks>
2025-01-30 20:41:52 -0300

    chore: refactor pre condition

--- packages/contracts-bedrock/test/invariants/interop/helpers/Handler.sol
+++ packages/contracts-bedrock/test/invariants/interop/helpers/Handler.sol
@@ -444,13 +444,13 @@ contract Handler is Setup {
         public
         initialize
     {
-        require(_id.chainId == block.chainid);
         require(_target != address(CROSS_L2_INBOX));
         require(_sender != address(L2_TO_L2_MESSENGER));
         require(!_isL1Contract(_target));
 
         // Ensure the id inputs are valid
         _id.origin = address(L2_TO_L2_MESSENGER);
+        _id.chainId == block.chainid;
 
         // Build the message
         bytes memory sentMessage = abi.encodePacked(
