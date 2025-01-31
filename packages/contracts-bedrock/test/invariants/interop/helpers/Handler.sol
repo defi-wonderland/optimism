fbdfaa4a8736db4e0a010ad714fbd765a1725a23
hexshire <hexshire@defi.sucks>
2025-01-31 09:49:56 -0300

    fix: wrong pre condition

--- packages/contracts-bedrock/test/invariants/interop/helpers/Handler.sol
+++ packages/contracts-bedrock/test/invariants/interop/helpers/Handler.sol
@@ -445,7 +445,7 @@ contract Handler is Setup {
         initialize
     {
         require(_target != address(CROSS_L2_INBOX));
-        require(_sender != address(L2_TO_L2_MESSENGER));
+        require(_target != address(L2_TO_L2_MESSENGER));
         require(!_isL1Contract(_target));
 
         // Ensure the id inputs are valid
