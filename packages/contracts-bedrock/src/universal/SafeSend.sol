c48cff5673ab6862c7b7b80281d59e0138e9de73
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-02-05 18:19:14 -0300

    chore: add todo

--- packages/contracts-bedrock/src/universal/SafeSend.sol
+++ packages/contracts-bedrock/src/universal/SafeSend.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.0;
 contract SafeSend {
     /// @param _recipient Account to send ETH to.
     constructor(address payable _recipient) payable {
+        // TODO: Confirm if this will be introduced.
         // Send any leftover Ether amount to the address(0)
         uint256 diff = address(this).balance - msg.value;
         if (diff != 0) payable(address(0)).transfer(diff);
