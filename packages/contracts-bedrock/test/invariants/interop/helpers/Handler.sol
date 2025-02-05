67dc5997fc2ef041d0ca2ae0529b9806026474eb
Hex <165055168+hexshire@users.noreply.github.com>
2025-02-05 18:57:03 -0300

    Update packages/contracts-bedrock/test/invariants/interop/helpers/Handler.sol
    Co-authored-by: Disco <131301107+0xDiscotech@users.noreply.github.com>

--- packages/contracts-bedrock/test/invariants/interop/helpers/Handler.sol
+++ packages/contracts-bedrock/test/invariants/interop/helpers/Handler.sol
@@ -456,7 +456,7 @@ contract Handler is Setup {
 
     /// @notice This function is used to send ether to the random addresses. To check how balance changes affect the
     /// behavior of contracts.
-    function handler_SendETH(address payable _to, uint256 _amount) public initialize {
+    function handler_safeSendETH(address payable _to, uint256 _amount) public initialize {
         _amount = clampBetween(_amount, 1, 10 ether);
         // Since the Medusa.callSequenceLength is set 50, the max amount of ether that can be sent is 50 * 10 ether.
         vm.deal(address(this), _amount);
