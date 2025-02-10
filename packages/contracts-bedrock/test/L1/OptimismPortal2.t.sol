324ce8c5f4b8d360a1910d102ba00ca6c76f731d
OneTony <onetony@defi.sucks>
2025-02-10 15:51:40 +0200

    fix: tests

--- packages/contracts-bedrock/test/L1/OptimismPortal2.t.sol
+++ packages/contracts-bedrock/test/L1/OptimismPortal2.t.sol
@@ -19,6 +19,7 @@ import { Constants } from "src/libraries/Constants.sol";
 import { Predeploys } from "src/libraries/Predeploys.sol";
 import { GasPayingToken } from "src/libraries/GasPayingToken.sol";
 import { AddressAliasHelper } from "src/vendor/AddressAliasHelper.sol";
+import { StaticConfig } from "src/libraries/StaticConfig.sol";
 import "src/dispute/lib/Types.sol";
 import "src/libraries/PortalErrors.sol";
 
@@ -321,6 +322,7 @@ contract OptimismPortal2_Test is CommonTest {
         // TODO(opcm upgrades): remove skip once upgrade path is implemented
         skipIfForkTest("OptimismPortal2_Test: gas paying token functionality DNE on op mainnet");
 
+        bytes memory data = StaticConfig.encodeSetGasPayingToken(_token, _decimals, _name, _symbol);
         vm.expectEmit(address(optimismPortal2));
         emit TransactionDeposited(
             0xDeaDDEaDDeAdDeAdDEAdDEaddeAddEAdDEAd0001,
@@ -331,12 +333,12 @@ contract OptimismPortal2_Test is CommonTest {
                 uint256(0), // value
                 uint64(200_000), // gasLimit
                 false, // isCreation,
-                abi.encodeCall(IL1Block.setGasPayingToken, (_token, _decimals, _name, _symbol))
+                abi.encodeCall(IL1Block.setConfig, (Types.ConfigType.GAS_PAYING_TOKEN, data))
             )
         );
 
         vm.prank(address(systemConfig));
-        optimismPortal2.setGasPayingToken({ _token: _token, _decimals: _decimals, _name: _name, _symbol: _symbol });
+        optimismPortal2.setConfig(Types.ConfigType.GAS_PAYING_TOKEN, data);
     }
 
     /// @notice Ensures that the deposit event is correct for the `setGasPayingToken`
@@ -362,11 +364,13 @@ contract OptimismPortal2_Test is CommonTest {
         bytes32 name = GasPayingToken.sanitize(_name);
         bytes32 symbol = GasPayingToken.sanitize(_symbol);
 
+        bytes memory data = StaticConfig.encodeSetGasPayingToken(_token, 18, name, symbol);
+
         vm.recordLogs();
 
         vm.deal(address(systemConfig), 100 ether);
         vm.prank(address(systemConfig));
-        optimismPortal2.setGasPayingToken({ _token: _token, _decimals: 18, _name: name, _symbol: symbol });
+        optimismPortal2.setConfig(Types.ConfigType.GAS_PAYING_TOKEN, data);
 
         vm.prank(Constants.DEPOSITOR_ACCOUNT, Constants.DEPOSITOR_ACCOUNT);
         optimismPortal2.depositTransaction({
@@ -374,7 +378,7 @@ contract OptimismPortal2_Test is CommonTest {
             _value: 0,
             _gasLimit: 200_000,
             _isCreation: false,
-            _data: abi.encodeCall(IL1Block.setGasPayingToken, (_token, 18, name, symbol))
+            _data: abi.encodeCall(IL1Block.setConfig, (Types.ConfigType.GAS_PAYING_TOKEN, data))
         });
 
         VmSafe.Log[] memory logs = vm.getRecordedLogs();
@@ -397,10 +401,12 @@ contract OptimismPortal2_Test is CommonTest {
         // TODO(opcm upgrades): remove skip once upgrade path is implemented
         skipIfForkTest("OptimismPortal2_Test: gas paying token functionality DNE on op mainnet");
 
+        bytes memory data = StaticConfig.encodeSetGasPayingToken(address(0), 0, "", "");
+
         vm.assume(_caller != address(systemConfig));
         vm.prank(_caller);
         vm.expectRevert(Unauthorized.selector);
-        optimismPortal2.setGasPayingToken({ _token: address(0), _decimals: 0, _name: "", _symbol: "" });
+        optimismPortal2.setConfig(Types.ConfigType.GAS_PAYING_TOKEN, data);
     }
 
     /// @dev Tests that `depositERC20Transaction` reverts when the gas paying token is ether.
