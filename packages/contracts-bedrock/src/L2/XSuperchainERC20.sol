e5f929be37934bb8a7c2dff6bf3040c9f513b1ad
0xchin <alanracciatti1220@gmail.com>
2025-02-12 10:28:09 -0300

    perf(xSupERC20): optimize uint256/2 check

--- packages/contracts-bedrock/src/L2/XSuperchainERC20.sol
+++ packages/contracts-bedrock/src/L2/XSuperchainERC20.sol
@@ -101,7 +101,7 @@ contract XSuperchainERC20 is SuperchainERC20, Ownable, IXERC20 {
      * @param _bridge The address of the bridge we are setting the limits too
      */
     function setLimits(address _bridge, uint256 _mintingLimit, uint256 _burningLimit) external onlyOwner {
-        if (_mintingLimit > (type(uint256).max / 2) || _burningLimit > (type(uint256).max / 2)) {
+        if (_mintingLimit > (type(uint256).max >> 1) || _burningLimit > (type(uint256).max >> 1)) {
             revert IXERC20_LimitsTooHigh();
         }
 
