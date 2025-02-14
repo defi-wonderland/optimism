e9a61067a48ec62a5992b2e6e5d8c01f919be7a4
0xchin <alanracciatti1220@gmail.com>
2025-02-14 11:19:31 -0300

    fix: test failing due to uncasted SuperchainERC20 contract

--- packages/contracts-bedrock/test/L2/XSuperchainERC20.t.sol
+++ packages/contracts-bedrock/test/L2/XSuperchainERC20.t.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
-import { Test } from "forge-std/Test.sol";
 import { Base, UnitNames, UnitMintBurn, UnitCreateParams } from "@xERC20/test/unit/XERC20.t.sol";
 import { SuperchainERC20Test } from "test/L2/SuperchainERC20.t.sol";
 import { MockXSuperchainERC20Implementation } from "test/mocks/XSuperchainERC20Implementation.sol";
+import { SuperchainERC20 } from "src/L2/SuperchainERC20.sol";
 
-contract XSuperchainERC20Test is Test, UnitNames, UnitMintBurn, UnitCreateParams, SuperchainERC20Test {
+contract XSuperchainERC20Test is UnitNames, UnitMintBurn, UnitCreateParams, SuperchainERC20Test {
     function setUp() public override(Base, SuperchainERC20Test) {
         MockXSuperchainERC20Implementation _xSuperchainERC20 = new MockXSuperchainERC20Implementation("Test", "TST", _owner);
         _xerc20 = _xSuperchainERC20;
-        superchainERC20 = _xSuperchainERC20;
+        superchainERC20 = SuperchainERC20(address(_xSuperchainERC20));
     }
 }
--- packages/contracts-bedrock/test/mocks/XSuperchainERC20Implementation.sol
+++ packages/contracts-bedrock/test/mocks/XSuperchainERC20Implementation.sol
@@ -7,4 +7,12 @@ import { XSuperchainERC20 } from "src/L2/XSuperchainERC20.sol";
 /// @notice Mock contract just to create tests over an implementation of the XSuperchainERC20 abstract contract.
 contract MockXSuperchainERC20Implementation is XSuperchainERC20 {
     constructor(string memory _name, string memory _symbol, address _factory) XSuperchainERC20(_name, _symbol, _factory) {}
+
+    function name() public pure override returns (string memory) {
+        return "Test";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "TST";
+    }
 }
