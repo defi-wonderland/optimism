3398b58934e48ca378a07b763c12671bd2dafccc
0xchin <alanracciatti1220@gmail.com>
2025-02-17 13:16:12 -0300

    feat: add IXSuperchainERC20

---
+++ packages/contracts-bedrock/interfaces/L2/XSuperchainERC20/IXSuperchainERC20.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Interfaces
+import { IERC7802 } from "interfaces/L2/IERC7802.sol";
+import { IXERC20 } from "interfaces/L2/IXERC20.sol";
+import { ISemver } from "interfaces/universal/ISemver.sol";
+import { IOwnable } from "interfaces/universal/IOwnable.sol";
+
+/// @title IXSuperchainERC20
+/// @notice This interface is available on the XSuperchainERC20 contract.
+/// @dev This interface is needed for the abstract XSuperchainERC20 implementation but is not part of the standard
+interface IXSuperchainERC20 is IERC7802, IXERC20, ISemver, IOwnable {
+    error Unauthorized();
+
+    function supportsInterface(bytes4 _interfaceId) external view returns (bool);
+
+    function __constructor__() external;
+}
