195e306dd1e5f7241f51a9ecf9f7c93c2fdec757
0xchin <alanracciatti1220@gmail.com>
2025-02-17 13:40:02 -0300

    feat: add xSupERC20 Adapter

---
+++ packages/contracts-bedrock/src/L2/XSuperchainERC20/XSuperchainERC20Adapter.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+// Libraries
+import { Predeploys } from "src/libraries/Predeploys.sol";
+import { Unauthorized } from "src/libraries/errors/CommonErrors.sol";
+
+// Interfaces
+import { ISemver } from "interfaces/universal/ISemver.sol";
+import { IERC7802, IERC165 } from "interfaces/L2/IERC7802.sol";
+
+/// @title XSuperchainERC20Adapter
+/// @notice TODO: Add description
+contract XSuperchainERC20Adapter is IERC7802, ISemver {
+    /// @notice Semantic version.
+    /// @custom:semver 1.0.0-beta.1
+    function version() external view virtual returns (string memory) {
+        return "1.0.0-beta.1";
+    }
+
+    /// @notice Allows the SuperchainTokenBridge to mint tokens.
+    /// @param _to     Address to mint tokens to.
+    /// @param _amount Amount of tokens to mint.
+    function crosschainMint(address _to, uint256 _amount) external {
+        if (msg.sender != Predeploys.SUPERCHAIN_TOKEN_BRIDGE) revert Unauthorized();
+
+        _mint(_to, _amount);
+
+        emit CrosschainMint(_to, _amount, msg.sender);
+    }
+
+    /// @notice Allows the SuperchainTokenBridge to burn tokens.
+    /// @param _from   Address to burn tokens from.
+    /// @param _amount Amount of tokens to burn.
+    function crosschainBurn(address _from, uint256 _amount) external {
+        if (msg.sender != Predeploys.SUPERCHAIN_TOKEN_BRIDGE) revert Unauthorized();
+
+        _burn(_from, _amount);
+
+        emit CrosschainBurn(_from, _amount, msg.sender);
+    }
+
+    /// @inheritdoc IERC165
+    function supportsInterface(bytes4 _interfaceId) public view virtual returns (bool) {
+        return _interfaceId == type(IERC7802).interfaceId || _interfaceId == type(IERC20).interfaceId
+            || _interfaceId == type(IERC165).interfaceId;
+    }
+}
