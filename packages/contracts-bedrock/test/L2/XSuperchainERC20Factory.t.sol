51086c6f01961560a184ee4949f81c5c9b40f2be
0xchin <alanracciatti1220@gmail.com>
2025-02-17 11:06:51 -0300

    feat: add xSuperchainERC20Factory

---
+++ packages/contracts-bedrock/test/L2/XSuperchainERC20Factory.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import { Base, UnitDeploy, XERC20FactoryForTest } from '@xERC20/test/unit/XERC20Factory.t.sol';
+import { XSuperchainERC20Factory } from 'src/L2/XSuperchainERC20Factory.sol';
+import {CREATE3} from 'isolmate/utils/CREATE3.sol';
+
+contract XSuperchainERC20FactoryForTest is XSuperchainERC20Factory {
+  function getDeployed(bytes32 _salt) public view returns (address _precomputedAddress) {
+    _precomputedAddress = CREATE3.getDeployed(_salt);
+  }
+}
+
+/// @title XSuperchainERC20FactoryTest
+/// @notice Contract for testing the XSuperchainERC20Factory contract.
+contract XSuperchainERC20FactoryTest is UnitDeploy {
+    XSuperchainERC20FactoryForTest public _xSuperchainERC20Factory;
+
+    /// @notice Sets up the test suite.
+    /// @dev We need to override the `setUp` function to use the `XSuperchainERC20Factory` contract
+    /// instead of the `xERC20` and `SuperchainERC20` contracts.
+    function setUp() public override(Base) {
+        _xerc20Factory = new XSuperchainERC20FactoryForTest();
+    }
+}
