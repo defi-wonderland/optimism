188e1883ed78d4a460a0d72732ced1a1e12d3ca1
0xchin <alanracciatti1220@gmail.com>
2025-02-17 11:11:40 -0300

    test: explicit conversion of FactoryForTest

--- packages/contracts-bedrock/test/L2/XSuperchainERC20Factory.t.sol
+++ packages/contracts-bedrock/test/L2/XSuperchainERC20Factory.t.sol
@@ -20,6 +20,7 @@ contract XSuperchainERC20FactoryTest is UnitDeploy {
     /// @dev We need to override the `setUp` function to use the `XSuperchainERC20Factory` contract
     /// instead of the `xERC20` and `SuperchainERC20` contracts.
     function setUp() public override(Base) {
-        _xerc20Factory = new XSuperchainERC20FactoryForTest();
+        _xSuperchainERC20Factory = new XSuperchainERC20FactoryForTest();
+        _xerc20Factory = XERC20FactoryForTest(_xSuperchainERC20Factory);
     }
 }
