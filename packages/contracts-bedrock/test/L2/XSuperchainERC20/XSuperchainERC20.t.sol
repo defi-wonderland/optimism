e4696796460512958bdf20973fb63fd8fac05bb7
0xchin <alanracciatti1220@gmail.com>
2025-02-18 15:23:14 -0300

    test: fix test failing on zero address input

--- packages/contracts-bedrock/test/L2/XSuperchainERC20/XSuperchainERC20.t.sol
+++ packages/contracts-bedrock/test/L2/XSuperchainERC20/XSuperchainERC20.t.sol
@@ -91,7 +91,7 @@ contract XSuperchainERC20Test is UnitNames, UnitMintBurn, UnitCreateParams {
         vm.assume(_from != ZERO_ADDRESS);
 
         // Ensure the caller is not the bridge
-        vm.assume(_caller != SUPERCHAIN_TOKEN_BRIDGE);
+        vm.assume(_caller != SUPERCHAIN_TOKEN_BRIDGE && _caller != ZERO_ADDRESS);
 
         // Bound `amount` to not surpass the xERC20 limits
         _amount = bound(_amount, 1, 1e40);
