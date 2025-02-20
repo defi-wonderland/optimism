c43adc50cbcc1fa1aa6967dd2bd3aebe57b4593f
0xchin <alanracciatti1220@gmail.com>
2025-02-20 15:08:18 -0300

    test: fix test name style

--- packages/contracts-bedrock/test/L2/CrosschainERC20/ERC7802Adapter.t.sol
+++ packages/contracts-bedrock/test/L2/CrosschainERC20/ERC7802Adapter.t.sol
@@ -32,13 +32,13 @@ contract ERC7802AdapterTest is Test {
     }
 
     /// @notice Tests the `constructor` sets the `XERC20` contract.
-    function test_constructor_setsXERC20() public {
+    function test_constructor_setXERC20_succeeds() public {
         // Ensure the `XERC20` contract is set
         assertEq(address(adapter.XERC20()), XERC20);
     }
 
     /// @notice Tests the `constructor` sets the `BRIDGE` address.
-    function test_constructor_setsBridge() public {
+    function test_constructor_setBridge_succeeds() public {
         // Ensure the `BRIDGE` address is set
         assertEq(address(adapter.BRIDGE()), BRIDGE);
     }
