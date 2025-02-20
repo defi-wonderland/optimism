4ed7a549b4e0acaa6ccde3172a033fe26dea467f
0xchin <alanracciatti1220@gmail.com>
2025-02-20 09:47:26 -0300

    chore: rename xSuperchainERC20 to CrosschainERC20

---
+++ packages/contracts-bedrock/interfaces/L2/CrosschainERC20/CrosschainERC20.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+// Interfaces
+import { IERC7802 } from "interfaces/L2/IERC7802.sol";
+import { IXERC20 } from "interfaces/L2/IXERC20.sol";
+import { ISemver } from "interfaces/universal/ISemver.sol";
+import { IOwnable } from "interfaces/universal/IOwnable.sol";
+
+/// @title ICrosschainERC20
+/// @notice This interface is available on the CrosschainERC20 contract.
+interface ICrosschainERC20 is IERC7802, IXERC20, ISemver, IOwnable {
+    error Unauthorized();
+
+    function supportsInterface(bytes4 _interfaceId) external view returns (bool);
+
+    function __constructor__(string memory _name, string memory _symbol, address _factory) external;
+}
