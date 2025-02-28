ce73aa73519ded26ad081dc18e621f0f733029ee
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-02-28 11:57:59 -0300

    fix: error string on chain assertions

--- packages/contracts-bedrock/scripts/deploy/ChainAssertions.sol
+++ packages/contracts-bedrock/scripts/deploy/ChainAssertions.sol
@@ -393,15 +393,15 @@ library ChainAssertions {
             require(portal.l2Sender() == Constants.DEFAULT_L2_SENDER, "CHECK-OP2-70");
             require(address(portal.ethLockbox()) == _contracts.ETHLockbox, "CHECK-OP2-80");
         } else {
-            require(address(portal.disputeGameFactory()) == address(0), "CHECK-OP2-80");
-            require(address(portal.systemConfig()) == address(0), "CHECK-OP2-90");
-            require(address(portal.superchainConfig()) == address(0), "CHECK-OP2-100");
-            require(portal.l2Sender() == address(0), "CHECK-OP2-110");
-            require(address(portal.ethLockbox()) == address(0), "CHECK-OP2-120");
+            require(address(portal.disputeGameFactory()) == address(0), "CHECK-OP2-90");
+            require(address(portal.systemConfig()) == address(0), "CHECK-OP2-100");
+            require(address(portal.superchainConfig()) == address(0), "CHECK-OP2-110");
+            require(portal.l2Sender() == address(0), "CHECK-OP2-120");
+            require(address(portal.ethLockbox()) == address(0), "CHECK-OP2-130");
         }
         // This slot is the custom gas token _balance and this check ensures
         // that it stays unset for forwards compatibility with custom gas token.
-        require(vm.load(address(portal), bytes32(uint256(61))) == bytes32(0), "CHECK-OP2-120");
+        require(vm.load(address(portal), bytes32(uint256(61))) == bytes32(0), "CHECK-OP2-140");
     }
 
     /// @notice Asserts that the ETHLockbox is setup correctly
