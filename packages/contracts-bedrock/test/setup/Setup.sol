c6750385538417745f12f58675e70d3fedf3f2e7
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-03-06 17:00:28 -0300

    fix: include lockbox on reinitialization test
    * fix: polish nits

--- packages/contracts-bedrock/test/setup/Setup.sol
+++ packages/contracts-bedrock/test/setup/Setup.sol
@@ -233,12 +233,7 @@ contract Setup {
 
         optimismPortal2 = IOptimismPortal(artifacts.mustGetAddress("OptimismPortalProxy"));
 
-        if (isForkTest() && deploy.cfg().useUpgradedFork()) {
-            // If we are on the last upgrade network, we can use the predeployed ETHLockbox for the fork test
-            ethLockbox = IETHLockbox(artifacts.mustGetAddress("ETHLockboxProxy"));
-        } else if (!isForkTest()) {
-            ethLockbox = IETHLockbox(artifacts.mustGetAddress("ETHLockboxProxy"));
-        }
+        ethLockbox = IETHLockbox(artifacts.mustGetAddress("ETHLockboxProxy"));
         systemConfig = ISystemConfig(artifacts.mustGetAddress("SystemConfigProxy"));
         l1StandardBridge = IL1StandardBridge(artifacts.mustGetAddress("L1StandardBridgeProxy"));
         l1CrossDomainMessenger = IL1CrossDomainMessenger(artifacts.mustGetAddress("L1CrossDomainMessengerProxy"));
