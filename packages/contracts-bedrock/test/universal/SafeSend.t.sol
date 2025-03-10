15a5afddc3e2a74ebca9e20193d3ddf896e262ea
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-03-10 16:14:54 -0300

    fix: add leftover check on safe send

--- packages/contracts-bedrock/test/universal/SafeSend.t.sol
+++ packages/contracts-bedrock/test/universal/SafeSend.t.sol
@@ -43,4 +43,34 @@ contract SafeSendTest is CommonTest {
         assertEq(alice.balance, aliceBalanceBefore - 100 ether);
         assertEq(bob.balance, bobBalanceBefore + 100 ether);
     }
+
+    /// @notice Tests that sending to a contract with leftover ETH succeeds.
+    function test_send_toContractWithLeftoverETH_succeeds() public {
+        // Deal ETH to the alice and bob addresses
+        uint256 leftoverETH = 10 ether;
+        uint256 safeSendValue = 100 ether;
+        vm.deal(alice, leftoverETH + safeSendValue);
+
+        // Get the SafeSend contract address to be deployed
+        address safeSend = vm.computeCreateAddress(alice, vm.getNonce(alice));
+
+        // Send some ETH to the contract address before it is deployed-- this will be the leftover ETH
+        vm.startPrank(alice);
+        payable(safeSend).transfer(leftoverETH);
+
+        // Get the balances before the deployment
+        uint256 zeroAddressBalanceBefore = address(0).balance;
+        uint256 aliceBalanceBefore = alice.balance;
+        uint256 bobBalanceBefore = bob.balance;
+
+        // Deploy the SafeSend contract
+        new SafeSend{ value: safeSendValue }(payable(bob));
+
+        // Assert
+        assertEq(address(safeSend).code.length, 0);
+        assertEq(address(safeSend).balance, 0);
+        assertEq(alice.balance, aliceBalanceBefore - safeSendValue);
+        assertEq(bob.balance, bobBalanceBefore + safeSendValue);
+        assertEq(address(0).balance, zeroAddressBalanceBefore + leftoverETH);
+    }
 }
