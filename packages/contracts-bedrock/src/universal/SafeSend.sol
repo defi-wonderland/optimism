e5449a1ba3e5a45a6ba00f585c0e08efc8fe2c05
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-03-10 17:27:41 -0300

    fix: transfer leftover to zero address on safe send

--- packages/contracts-bedrock/src/universal/SafeSend.sol
+++ packages/contracts-bedrock/src/universal/SafeSend.sol
@@ -6,6 +6,11 @@ pragma solidity ^0.8.0;
 contract SafeSend {
     /// @param _recipient Account to send ETH to.
     constructor(address payable _recipient) payable {
+        /// NOTE: Send any ETH leftover to zero address, to avoid any potential issues derived from
+        ///       a mismatch between the expected and actual ETH amount sent to the recipient.
+        uint256 diff = address(this).balance - msg.value;
+        if (diff != 0) payable(address(0)).transfer(diff);
+
         selfdestruct(_recipient);
     }
 }
