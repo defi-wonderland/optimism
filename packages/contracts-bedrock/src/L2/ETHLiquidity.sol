593e8bf50ffdbb3d421b260dbe9a76802d94a735
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-03-11 17:25:38 -0300

    chore: pre-pr

--- packages/contracts-bedrock/src/L2/ETHLiquidity.sol
+++ packages/contracts-bedrock/src/L2/ETHLiquidity.sol
@@ -25,8 +25,8 @@ contract ETHLiquidity is ISemver {
     event LiquidityMinted(address indexed caller, uint256 value);
 
     /// @notice Semantic version.
-    /// @custom:semver 1.0.0-beta.6
-    string public constant version = "1.0.0-beta.6";
+    /// @custom:semver 1.0.0-beta.7
+    string public constant version = "1.0.0-beta.7";
 
     /// @notice Allows an address to lock ETH liquidity into this contract.
     function burn() external payable {
