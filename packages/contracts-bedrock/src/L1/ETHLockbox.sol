2f08d9e407ebbf6d0a00e8b3a7521c971ef34aed
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-03-18 17:53:40 -0300

    chore: pre pr

--- packages/contracts-bedrock/src/L1/ETHLockbox.sol
+++ packages/contracts-bedrock/src/L1/ETHLockbox.sol
@@ -74,9 +74,9 @@ contract ETHLockbox is ProxyAdminOwnedBase, Initializable, ISemver {
     mapping(IETHLockbox => bool) public authorizedLockboxes;
 
     /// @notice Semantic version.
-    /// @custom:semver 1.0.0
+    /// @custom:semver 1.0.1
     function version() public view virtual returns (string memory) {
-        return "1.0.0";
+        return "1.0.1";
     }
 
     /// @notice Constructs the ETHLockbox contract.
