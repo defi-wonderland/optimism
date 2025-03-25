633909a79b32a3d9cdc9e204a46166948f28b90b
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-03-25 12:50:43 -0300

    fix: stack underflow permit2 issue

--- packages/contracts-bedrock/test/invariants/interop/helpers/Actors.sol
+++ packages/contracts-bedrock/test/invariants/interop/helpers/Actors.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import { Predeploys } from "src/libraries/Predeploys.sol";
 import { GhostStorage } from "./GhostStorage.sol";
 import { vm } from "../utils/VM.sol";
 
 // Actors handler, reusing the msg.sender used by Medusa (defined in the json)
 // and tracking them, allowing to aggregate balances for instance.
 contract Actors {
+    /// NOTE: Leaving here to avoid stack underflow failures while calling `Permit2Mock` contract.
+    address public immutable SUPERCHAIN_TOKEN_BRIDGE = Predeploys.SUPERCHAIN_TOKEN_BRIDGE;
+    address public immutable L2_TO_L2_CROSS_DOMAIN_MESSENGER = Predeploys.L2_TO_L2_CROSS_DOMAIN_MESSENGER;
+    address public immutable SUPERCHAIN_WETH = Predeploys.SUPERCHAIN_WETH;
+    address public immutable CROSS_L2_INBOX = Predeploys.CROSS_L2_INBOX;
+
     event ActorsLog(string);
 
     function directCall(
