b550c5b8df720dbfec77520e3a7f38687c366f32
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-03-27 21:12:57 -0300

    fix: cross l2 inbox mock interface

--- packages/contracts-bedrock/test/invariants/interop/mocks/ICrossL2InboxWithSlotWarming.sol
+++ packages/contracts-bedrock/test/invariants/interop/mocks/ICrossL2InboxWithSlotWarming.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import { Identifier } from "interfaces/L2/ICrossL2Inbox.sol";
+import { ICrossL2Inbox } from "interfaces/L2/ICrossL2Inbox.sol";
 
-interface ICrossL2InboxWithSlotWarming {
+interface ICrossL2InboxWithSlotWarming is ICrossL2Inbox {
     function warmSlot(bytes32 _slot) external view returns (uint256 res_);
 
     function isWarm(bytes32 _slot) external view returns (bool isWarm_, uint256 value_);
