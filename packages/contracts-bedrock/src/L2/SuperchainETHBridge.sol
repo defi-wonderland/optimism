6947065096c83d6eda2199652c4c1884c890999e
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2025-04-25 16:19:24 -0300

    fix: logic and namig vars

--- packages/contracts-bedrock/src/L2/SuperchainETHBridge.sol
+++ packages/contracts-bedrock/src/L2/SuperchainETHBridge.sol
@@ -19,12 +19,21 @@ import { IETHLiquidity } from "interfaces/L2/IETHLiquidity.sol";
 contract SuperchainETHBridge is ISemver {
     using FixedPointMathLib for uint256;
 
+    /// @notice The bucket checkpoint struct
+    /// @param timestamp The timestamp of the last bucket usage checkpoint
+    /// @param usage The amount of ETH used from the bucket since the last checkpoint
+    /// @dev Packing both values in
+    struct BucketCheckpoint {
+        uint128 timestamp;
+        uint128 usage;
+    }
+
     /// @notice Thrown when attempting to relay a message and the cross domain message sender is not
     /// SuperchainETHBridge.
     error InvalidCrossDomainSender();
 
-    /// @notice Thrown when the rate limit is exceeded.
-    error RateLimitExceeded();
+    /// @notice Thrown when there is no enough bucket availability for the given ETH amount.
+    error NoBucketAvailability();
 
     /// @notice Thrown when the amount is too high.
     error AmountTooHigh();
@@ -44,61 +53,75 @@ contract SuperchainETHBridge is ISemver {
     event RelayETH(address indexed from, address indexed to, uint256 amount, uint256 source);
 
     /// @notice Semantic version.
-    /// @custom:semver 1.0.1
-    string public constant version = "1.0.1";
+    /// @custom:semver 1.1.0
+    string public constant version = "1.1.0";
 
+    /// @notice The timestamp of the rate limit activation, used to increase the bucket capacity as time passes
     uint256 public constant ETH_RATE_LIMIT_ACTIVATION = 1745340856; // 22 apr
 
-    /// Fee calculation parameters
+    // TODO: Define proper fee and bucket config values
+    /// @notice The maximum fee percentage of the ETH amount calculation
     uint256 public constant MAX_FEE_PERCENTAGE = 0.02e18; // 2%
+    /// @notice The curve exponent for the fee calculation
     uint256 public constant CURVE_EXPONENT = 8;
+    /// @notice The base fee for the fee calculation
     uint256 public constant BASE_FEE = 100_000;
+    /// @notice The refill time window for the bucket
     uint256 public constant REFILL_TIME_WINDOW = 1 hours;
 
-    /// The maximum amount of ETH that can be sent in a single transaction
-    uint256 public maxAmount = 70 * bucketCapacity() / 100; // 70% of the bucket capacity
-    /// TODO: These two vars could be packed into a single uint256 slot
-    uint256 public lastRefillTime = block.timestamp;
-    uint256 public lastBucketUsage;
+    /// @notice The maximum amount of ETH that can be sent in a single transaction, 70% of the bucket capacity
+    uint256 public maxTxETHAmount = 70 * bucketCapacity() / 100;
 
+    /// @notice The last checkpoint of the bucket usage state
+    BucketCheckpoint public lastBucketCheckpoint = BucketCheckpoint({ timestamp: uint128(block.timestamp), usage: 0 });
+
+    /// @notice Returns the bucket capacity based on the time since the rate limit activation
+    // TODO: Define proper bucket capacity values
     function bucketCapacity() public view returns (uint256) {
-        if (block.timestamp - ETH_RATE_LIMIT_ACTIVATION > 7 days) {
-            return 500 ether;
+        if (block.timestamp - ETH_RATE_LIMIT_ACTIVATION > 30 days) {
+            return 2000 ether;
         } else if (block.timestamp - ETH_RATE_LIMIT_ACTIVATION > 14 days) {
             return 1000 ether;
-        } else if (block.timestamp - ETH_RATE_LIMIT_ACTIVATION > 21 days) {
-            return 2000 ether;
+        } else if (block.timestamp - ETH_RATE_LIMIT_ACTIVATION > 7 days) {
+            return 500 ether;
+        } else if (block.timestamp - ETH_RATE_LIMIT_ACTIVATION > 1 days) {
+            return 100 ether;
         } else {
             return 10_000 ether;
         }
     }
 
-    /// @dev bucketAvailable = bucketCapacity - bucketUsage
-    /// @dev The bucketUsage is calculated based on the the time elapsed since the last refill
-    function bucketAvailable() public view returns (uint256 bucketAvailable_) {
-        if (lastBucketUsage == 0) return bucketAvailable_ = bucketCapacity();
+    /// @notice Returns the bucket available amount and the bucket refill amount since the last bucket usage checkpoint
+    /// @return bucketAvailable_ The available bucket capacity at the current timestamp
+    /// @return bucketRefillAmount_ The potential refill amount since the last bucket usage checkpoint
+    function bucketAvailable() public view returns (uint256 bucketAvailable_, uint256 bucketRefillAmount_) {
+        uint256 elapsedTime = block.timestamp - lastBucketCheckpoint.timestamp;
+        if (elapsedTime > REFILL_TIME_WINDOW) {
+            return (bucketAvailable_ = bucketCapacity(), bucketRefillAmount_ = lastBucketCheckpoint.usage);
+        }
 
         // Calculate the refill amount based on the time elapsed since the last refill
-        uint256 refillRate = bucketCapacity() / REFILL_TIME_WINDOW;
-        uint256 refillAmount = (block.timestamp - lastRefillTime) * refillRate;
+        uint256 bucketRefillRate = bucketCapacity() / REFILL_TIME_WINDOW;
+        bucketRefillAmount_ = elapsedTime * bucketRefillRate;
 
         // Calculate the new bucket usage
         uint256 newBucketUsage;
-        if (refillAmount > lastBucketUsage) {
+        if (bucketRefillAmount_ > lastBucketCheckpoint.usage) {
             // The bucket is fully refilled
             newBucketUsage = 0;
         } else {
             // The bucket is partially refilled
-            newBucketUsage = lastBucketUsage - refillAmount;
+            newBucketUsage = lastBucketCheckpoint.usage - bucketRefillAmount_;
         }
 
         // Return the current bucket availibility
         bucketAvailable_ = bucketCapacity() - newBucketUsage;
     }
 
+    // TODO: Move to a library
     function calculateFee(uint256 amount) public view returns (uint256) {
         // Normalize amount to [0, 1] in 18 decimals
-        uint256 normalized = amount.divWad(maxAmount);
+        uint256 normalized = amount.divWad(maxTxETHAmount);
 
         // Raise to the exponent (e.g., x^3)
         uint256 powered = normalized.rpow(CURVE_EXPONENT, 1e18);
@@ -116,7 +139,7 @@ contract SuperchainETHBridge is ISemver {
     /// @return msgHash_ Hash of the message sent.
     function sendETH(address _to, uint256 _chainId) external payable returns (bytes32 msgHash_) {
         if (_to == address(0)) revert ZeroAddress();
-        if (msg.value > maxAmount) revert AmountTooHigh();
+        if (msg.value > maxTxETHAmount) revert AmountTooHigh();
 
         uint256 amountToSend = msg.value - calculateFee(msg.value);
 
@@ -139,16 +162,26 @@ contract SuperchainETHBridge is ISemver {
     function relayETH(address _from, address _to, uint256 _amount) external {
         if (msg.sender != Predeploys.L2_TO_L2_CROSS_DOMAIN_MESSENGER) revert Unauthorized();
 
+        // Check that the amount is within the rate limit
+        if (_amount > maxTxETHAmount) revert AmountTooHigh();
+        (uint256 bucketAvailableAmount, uint256 bucketRefillAmount) = bucketAvailable();
+        if (_amount > bucketAvailableAmount) revert NoBucketAvailability();
+
+        // Check that the cross domain message sender is valid
         (address crossDomainMessageSender, uint256 source) =
             IL2ToL2CrossDomainMessenger(Predeploys.L2_TO_L2_CROSS_DOMAIN_MESSENGER).crossDomainMessageContext();
-
         if (crossDomainMessageSender != address(this)) revert InvalidCrossDomainSender();
+
+        // Update the bucket usage and timestamp
+        if (bucketAvailableAmount == bucketCapacity()) {
+            lastBucketCheckpoint.usage = uint128(_amount);
+        } else {
+            lastBucketCheckpoint.usage += uint128(_amount) - uint128(bucketRefillAmount);
+        }
+        lastBucketCheckpoint.timestamp = uint128(block.timestamp);
 
         // NOTE: 'mint' will soon change to 'withdraw'.
         IETHLiquidity(Predeploys.ETH_LIQUIDITY).mint(_amount);
-
-        if (_amount > bucketAvailable()) revert RateLimitExceeded();
-        lastBucketUsage += _amount;
 
         // This is a forced ETH send to the recipient, the recipient should NOT expect to be called.
         new SafeSend{ value: _amount }(payable(_to));
