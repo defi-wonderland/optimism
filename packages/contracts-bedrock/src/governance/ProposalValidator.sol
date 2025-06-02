2c681acbc936f3be2ab8441ee14c207fe56b7f0d
0xchin <77933451+0xChin@users.noreply.github.com>
2025-06-02 10:00:49 -0300

    chore: improve code formatting

--- packages/contracts-bedrock/src/governance/ProposalValidator.sol
+++ packages/contracts-bedrock/src/governance/ProposalValidator.sol
@@ -66,8 +66,7 @@ contract ProposalValidator is OwnableUpgradeable, ReinitializableBase, ISemver {
     }
 
     /// @notice Struct for storing explicit data for each proposal type.
-    /// @param requiredApprovals The number of approvals each proposal
-    /// type requires in order to be able to move for voting.
+    /// @param requiredApprovals The number of approvals each proposal type requires in order to be able to move for voting.
     /// @param proposalTypeConfigurator The voting module each proposal type must use.
     struct ProposalTypeData {
         uint256 requiredApprovals;
