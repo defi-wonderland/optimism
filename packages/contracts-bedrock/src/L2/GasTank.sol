ca7ea6003e8b3deb16e86b345886c887d193444c
GhostLee <ghost@wonderland.xyz>
2025-06-04 21:41:32 -0400

    feat: add natspec

--- packages/contracts-bedrock/src/L2/GasTank.sol
+++ packages/contracts-bedrock/src/L2/GasTank.sol
@@ -7,21 +7,36 @@ import { IGasTank } from "interfaces/L2/IGasTank.sol";
 import { Predeploys } from "src/libraries/Predeploys.sol";
 import { SafeSend } from "src/universal/SafeSend.sol";
 
+/// @title GasTank
+/// @notice Allows users to deposit gas funds to compensate relayers for executing cross chain transactions
 contract GasTank is IGasTank {
+    /// @notice The maximum amount of funds that can be deposited into the gas tank
     uint256 public constant MAX_DEPOSIT = 0.01 ether;
+
+    /// @notice The delay before a withdrawal can be finalized
     uint256 public constant WITHDRAWAL_DELAY = 7 days;
-    // TODO: Calculate claim overhead
+
+    /// @notice The gas cost of claiming a receipt
     uint256 public constant CLAIM_OVERHEAD = 100_000;
 
+    /// @notice The cross domain messenger
     IL2ToL2CrossDomainMessenger public constant MESSENGER =
         IL2ToL2CrossDomainMessenger(Predeploys.L2_TO_L2_CROSS_DOMAIN_MESSENGER);
 
-    mapping(address => uint256) public balanceOf;
-    mapping(address gasProvider => Withdrawal) public withdrawals;
-    mapping(bytes32 => bool) public claimed;
-    mapping(address gasProvider => mapping(bytes32 msgHash => bool)) public flaggedMessages;
+    /// @notice The balance of each gas provider
+    mapping(address gasProvider => uint256 balance) public balanceOf;
 
-    // Deposit funds into the gas tank, from which the relayer can claim the repayment after relaying
+    /// @notice The current withdrawal of each gas provider
+    mapping(address gasProvider => Withdrawal) public withdrawals;
+
+    /// @notice The claimed messages
+    mapping(bytes32 rootMsgHash => bool claimed) public claimed;
+
+    /// @notice The flagged messages for relaying
+    mapping(address gasProvider => mapping(bytes32 msgHash => bool flagged)) public flaggedMessages;
+
+    /// @notice Deposits funds into the gas tank, from which the relayer can claim the repayment after relaying
+    /// @param _to The address to deposit the funds to
     function deposit(address _to) external payable {
         uint256 newBalance = balanceOf[_to] + msg.value;
 
@@ -31,6 +46,8 @@ contract GasTank is IGasTank {
         emit Deposit(_to, msg.value);
     }
 
+    /// @notice Initiates a withdrawal of funds from the gas tank
+    /// @param amount The amount of gas funds to withdraw
     function initiateWithdrawal(uint256 amount) external {
         // Ensure the caller has enough balance
         if (balanceOf[msg.sender] < amount) revert InsufficientBalance();
@@ -42,6 +59,8 @@ contract GasTank is IGasTank {
         emit WithdrawalInitiated(msg.sender, amount);
     }
 
+    /// @notice Finalizes a withdrawal of funds from the gas tank
+    /// @param to The address to withdraw the funds to
     function finalizeWithdrawal(address to) external {
         Withdrawal memory withdrawal = withdrawals[msg.sender];
 
@@ -61,13 +80,17 @@ contract GasTank is IGasTank {
         emit WithdrawalFinalized(msg.sender, to, amount);
     }
 
-    // Flag a message into the gas tank so the relayer is aware of it, and can claim the funds after relaying
+    /// @notice Flags a message into the gas tank so the relayer is aware of it, and can claim the funds after relaying
+    /// @param rootMessageHash The hash of the root message
     function flag(bytes32 rootMessageHash) external {
         flaggedMessages[msg.sender][rootMessageHash] = true;
         emit Flagged(rootMessageHash, msg.sender);
     }
 
-    // Claim repayment for a relayed message
+    /// @notice Claims repayment for a relayed message
+    /// @param id The identifier of the message
+    /// @param gasProvider The address of the gas provider
+    /// @param payload The payload of the message
     function claim(Identifier calldata id, address gasProvider, bytes calldata payload) external {
         // Ensure the origin is the messenger
         if (id.origin != address(MESSENGER)) revert InvalidOrigin();
@@ -85,7 +108,6 @@ contract GasTank is IGasTank {
         // Compute total cost (adding the overhead of this claim)
         uint256 claimCost = CLAIM_OVERHEAD * block.basefee;
         uint256 cost = relayCost + claimCost;
-        // TODO: Make it more flexible so to allow partial repayment, but tracking the claim as partially repaid
         if (balanceOf[gasProvider] < cost) revert InsufficientBalance();
 
         // Ensure the original outbound message was sent from this chain
@@ -104,7 +126,12 @@ contract GasTank is IGasTank {
         emit Claimed(msgHash, relayer, gasProvider, rootMsgHash, cost);
     }
 
-    // Decode the payload of the RelayedMessageGasReceipt event
+    /// @notice Decodes the payload of the RelayedMessageGasReceipt event
+    /// @param payload The payload of the RelayedMessageGasReceipt event
+    /// @return msgHash The hash of the message
+    /// @return rootMsgHash The hash of the root message
+    /// @return relayer The address of the relayer
+    /// @return relayCost The cost of the relay
     function decodeGasReceiptPayload(bytes calldata payload)
         public
         pure
