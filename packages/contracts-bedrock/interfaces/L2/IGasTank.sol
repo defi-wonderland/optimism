ba7b1d8b949f92d7b9bf3560ad06d80bf607d9d3
hexshire <hex@wonderland.xyz>
2025-06-12 16:25:18 -0300

    fix: gas tank interface

--- packages/contracts-bedrock/interfaces/L2/IGasTank.sol
+++ packages/contracts-bedrock/interfaces/L2/IGasTank.sol
@@ -49,7 +49,7 @@ interface IGasTank {
     function initiateWithdrawal(uint256 amount) external;
     function finalizeWithdrawal(address to) external;
     function flag(bytes32 originMessageHash) external;
-    function relayMessage(Identifier[] calldata _id, bytes[] calldata _sentMessage) external;
+    function relayMessage(Identifier calldata _id, bytes calldata _sentMessage) external;
     function claim(Identifier calldata id, address gasProvider, bytes calldata payload) external;
     function decodeGasReceiptPayload(bytes calldata payload)
         external
