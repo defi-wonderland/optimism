cee0bc23c1a704dd45b4c7589e8cfab976c9a069
funkornaut001 <107587461+funkornaut001@users.noreply.github.com>
2025-08-14 17:17:16 -0500

    chore: pre-pr

--- packages/contracts-bedrock/scripts/deploy/DeployConfig.s.sol
+++ packages/contracts-bedrock/scripts/deploy/DeployConfig.s.sol
@@ -123,8 +123,9 @@ contract DeployConfig is Script {
         governanceTokenOwner = stdJson.readAddress(_json, "$.governanceTokenOwner");
         feeSplitterRevenueShareRecipient =
             _readOr(_json, "$.feeSplitterRevenueShareRecipient", address(0x1234567890123456789012345678901234567890));
-        feeSplitterRevenueRemainderRecipient =
-            _readOr(_json, "$.feeSplitterRevenueRemainderRecipient", address(0x0987654321098765432109876543210987654321));
+        feeSplitterRevenueRemainderRecipient = _readOr(
+            _json, "$.feeSplitterRevenueRemainderRecipient", address(0x0987654321098765432109876543210987654321)
+        );
         feeSplitterFeeDisbursementInterval = _readOr(_json, "$.feeSplitterFeeDisbursementInterval", uint256(86400));
         feeSplitterNetFeeShareBP = _readOr(_json, "$.feeSplitterNetFeeShareBP", uint256(1500));
         feeSplitterGrossFeeShareBP = _readOr(_json, "$.feeSplitterGrossFeeShareBP", uint256(250));
