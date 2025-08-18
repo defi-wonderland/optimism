3333aec6a3715e47eb3c156de5fd61e1f3775545
hexshire <hex@wonderland.xyz>
2025-08-18 15:59:51 -0300

    chore(cgt): consolidate duplicated flags

--- op-chain-ops/interopgen/recipe.go
+++ op-chain-ops/interopgen/recipe.go
@@ -243,7 +243,7 @@ func (r *InteropDevL2Recipe) build(l1ChainID uint64, addrs devkeys.Addresses) (*
 				GasPriceOracleBlobBaseFeeScalar: 810949,
 			},
 			GasTokenDeployConfig: genesis.GasTokenDeployConfig{
-				UseCustomGasToken: false,
+				IsCustomGasToken: false,
 			},
 			OperatorDeployConfig: genesis.OperatorDeployConfig{
 				P2PSequencerAddress: sequencerP2P,
