c8923912d8b9bb89da09d624ad8006a1ff1dc1a8
funkornaut001 <107587461+funkornaut001@users.noreply.github.com>
2025-08-27 13:47:09 -0500

    fix: bump versions

--- packages/contracts-bedrock/src/L2/SequencerFeeVault.sol
+++ packages/contracts-bedrock/src/L2/SequencerFeeVault.sol
@@ -16,8 +16,8 @@ import { ISemver } from "interfaces/universal/ISemver.sol";
 /// @notice The SequencerFeeVault is the contract that holds any fees paid to the Sequencer during
 ///         transaction processing and block production.
 contract SequencerFeeVault is FeeVault, ISemver {
-    /// @custom:semver 1.5.2
-    string public constant version = "1.5.2";
+    /// @custom:semver 1.6.0
+    string public constant version = "1.6.0";
 
     /// @notice Constructs the SequencerFeeVault contract.
     /// @param _recipient           Wallet that will receive the fees.
