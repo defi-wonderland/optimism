5cfc17664e06330b2d23633dc345ed5653ba2ae8
funkornaut001 <107587461+funkornaut001@users.noreply.github.com>
2025-08-28 08:20:08 -0500

    fix: l2 genesis fee splitter deployment

--- packages/contracts-bedrock/scripts/L2Genesis.s.sol
+++ packages/contracts-bedrock/scripts/L2Genesis.s.sol
@@ -579,11 +579,10 @@ contract L2Genesis is Script {
     /// @notice This predeploy is following the safety invariant #1.
     function setFeeSplitter(Input memory _input) internal {
         address impl = _setImplementationCode(Predeploys.FEE_SPLITTER);
-        vm.startPrank(_input.opChainProxyAdminOwner);
         // Initialize the implementation with address(0) for addresses
         IFeeSplitter(payable(impl)).initialize(
             ISharesCalculator(address(0)),
-            uint128(_input.feeSplitterFeeDisbursementInterval)
+            uint128(0)
         );
 
         // Initialize the proxy with the actual values
@@ -591,7 +590,6 @@ contract L2Genesis is Script {
             ISharesCalculator(_input.feeSplitterSharesCalculator),
             uint128(_input.feeSplitterFeeDisbursementInterval)
         );
-        vm.stopPrank();
     }
 
     /// @notice Sets the bytecode in state
