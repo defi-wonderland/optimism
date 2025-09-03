b6f3ce4974afecb41d9452fd27ac6eec572f90ac
niha <205694301+0xniha@users.noreply.github.com>
2025-09-03 17:40:09 -0300

    chore: add set cgt flag on l1block in genesis

--- packages/contracts-bedrock/test/L2/L2ToL1MessagePasserCGT.t.sol
+++ packages/contracts-bedrock/test/L2/L2ToL1MessagePasserCGT.t.sol
@@ -18,11 +18,6 @@ contract L2ToL1MessagePasserCGT_TestInit is CommonTest {
     function setUp() public override {
         super.enableCustomGasToken();
         super.setUp();
-
-        // Manually activate custom gas token since we removed the constructor parameter
-        address depositor = l1Block.DEPOSITOR_ACCOUNT();
-        vm.prank(depositor);
-        l1Block.setCustomGasToken();
     }
 }
 
