3abb3cf8a04f92b250442b8ce78215bd4892ebdd
hexshire <hex@wonderland.xyz>
2025-09-04 14:23:02 -0300

    test(cgt): fix failing tests

--- packages/contracts-bedrock/test/L2/LiquidityController.t.sol
+++ packages/contracts-bedrock/test/L2/LiquidityController.t.sol
@@ -142,6 +142,7 @@ contract LiquidityController_Mint_Test is LiquidityController_TestInit {
         public
         isAuthorizedMinter(authorizedMinter)
     {
+        vm.assume(_to != address(nativeAssetLiquidity));
         _amount = bound(_amount, 1, address(nativeAssetLiquidity).balance);
 
         // Record initial balances
@@ -164,6 +165,7 @@ contract LiquidityController_Mint_Test is LiquidityController_TestInit {
 
     /// @notice Tests that the mint function reverts when called by unauthorized address.
     function testFuzz_mint_fromUnauthorizedCaller_fails(address _caller, address _to, uint256 _amount) public {
+        vm.assume(_to != address(nativeAssetLiquidity));
         _amount = bound(_amount, 1, type(uint248).max);
 
         uint256 nativeAssetBalanceBefore = address(nativeAssetLiquidity).balance;
