0956548047e1f800a27a4976fb304cf0901293d4
Chiin <77933451+0xChin@users.noreply.github.com>
2025-09-25 12:16:06 -0300

    chore: fix comments
    Co-authored-by: Disco <131301107+0xDiscotech@users.noreply.github.com>
    Signed-off-by: Chiin <77933451+0xChin@users.noreply.github.com>

--- packages/contracts-bedrock/src/L1/FeesDepositor.sol
+++ packages/contracts-bedrock/src/L1/FeesDepositor.sol
@@ -115,7 +115,7 @@ contract FeesDepositor is ProxyAdminOwnedBase, Initializable, ReinitializableBas
     }
 
     /// @notice Updates the L2 recipient for the deposit transaction.
-    /// @dev The L2 recipient MUST be able to receive ether or FeesDepositor#receive will fail.
+    /// @dev The L2 recipient MUST be able to receive ether or the deposit on L2 will fail.
     /// @param _newL2Recipient The new L2 recipient.
     function setL2Recipient(address _newL2Recipient) external {
         _assertOnlyProxyAdminOwner();
