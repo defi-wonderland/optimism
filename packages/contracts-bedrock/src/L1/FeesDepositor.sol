2a019b38a06f3cc5aea87051282d345388f8661f
Chiin <77933451+0xChin@users.noreply.github.com>
2025-09-25 12:23:54 -0300

    chore: fix
    Co-authored-by: Disco <131301107+0xDiscotech@users.noreply.github.com>
    Signed-off-by: Chiin <77933451+0xChin@users.noreply.github.com>

--- packages/contracts-bedrock/src/L1/FeesDepositor.sol
+++ packages/contracts-bedrock/src/L1/FeesDepositor.sol
@@ -93,7 +93,7 @@ contract FeesDepositor is ProxyAdminOwnedBase, Initializable, ReinitializableBas
     }
 
     /// @notice Receives ETH and deposits it to the L2 recipient through the portal when the threshold is reached.
-    /// @dev Be aware that when the DepositTransaction is sent, the `from` address will be alias of this contract address.
+    /// @dev Be aware that when the DepositTransaction is sent, the `from` address will be the alias of this contract address.
     receive() external payable {
         uint256 balance = address(this).balance;
         emit FundsReceived(msg.sender, msg.value, balance);
