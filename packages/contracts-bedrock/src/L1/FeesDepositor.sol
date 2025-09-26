72ebafd1408bfb97908158344cb5d2b8f029fe9c
Chiin <77933451+0xChin@users.noreply.github.com>
2025-09-26 16:30:10 -0300

    fix: remove withdrawal data (#617)

--- packages/contracts-bedrock/src/L1/FeesDepositor.sol
+++ packages/contracts-bedrock/src/L1/FeesDepositor.sol
@@ -23,9 +23,6 @@ contract FeesDepositor is ProxyAdminOwnedBase, Initializable, ReinitializableBas
     /// @notice The gas limit for the deposit transaction.
     uint64 public gasLimit;
 
-    /// @notice The data for the deposit transaction.
-    bytes public depositData;
-
     /// @notice Emitted when fees are received.
     /// @param sender The sender of the fees.
     /// @param amount The amount of fees received.
@@ -52,11 +49,6 @@ contract FeesDepositor is ProxyAdminOwnedBase, Initializable, ReinitializableBas
     /// @param newGasLimit The new gas limit.
     event GasLimitUpdated(uint64 oldGasLimit, uint64 newGasLimit);
 
-    /// @notice Emitted when the deposit data is updated.
-    /// @param oldDepositData The old deposit data.
-    /// @param newDepositData The new deposit data.
-    event DepositDataUpdated(bytes oldDepositData, bytes newDepositData);
-
     /// @notice Semantic version.
     /// @custom:semver 1.0.0
     string public constant version = "1.0.0";
@@ -71,13 +63,11 @@ contract FeesDepositor is ProxyAdminOwnedBase, Initializable, ReinitializableBas
     /// @param _l2Recipient The L2 recipient of the fees.
     /// @param _portal The portal contract.
     /// @param _gasLimit The gas limit for the deposit transaction.
-    /// @param _depositData The deposit data for the deposit transaction.
     function initialize(
         uint96 _minDepositAmount,
         address _l2Recipient,
         IOptimismPortal _portal,
-        uint64 _gasLimit,
-        bytes memory _depositData
+        uint64 _gasLimit
     )
         external
         reinitializer(initVersion())
@@ -89,18 +79,18 @@ contract FeesDepositor is ProxyAdminOwnedBase, Initializable, ReinitializableBas
         minDepositAmount = _minDepositAmount;
         l2Recipient = _l2Recipient;
         gasLimit = _gasLimit;
-        depositData = _depositData;
     }
 
     /// @notice Receives ETH and deposits it to the L2 recipient through the portal when the threshold is reached.
-    /// @dev Be aware that when the DepositTransaction is sent, the `from` address will be the alias of this contract address.
+    /// @dev Be aware that when the DepositTransaction is sent, the `from` address will be the alias of this contract
+    /// address.
     receive() external payable {
         uint256 balance = address(this).balance;
         emit FundsReceived(msg.sender, msg.value, balance);
 
         if (balance >= minDepositAmount) {
             address recipient = l2Recipient;
-            portal.depositTransaction{ value: balance }(recipient, balance, gasLimit, false, depositData);
+            portal.depositTransaction{ value: balance }(recipient, balance, gasLimit, false, "");
             emit FeesDeposited(recipient, balance);
         }
     }
@@ -131,14 +121,5 @@ contract FeesDepositor is ProxyAdminOwnedBase, Initializable, ReinitializableBas
         uint64 oldGasLimit = gasLimit;
         gasLimit = _newGasLimit;
         emit GasLimitUpdated(oldGasLimit, _newGasLimit);
-    }
-
-    /// @notice Updates the deposit data.
-    /// @param _newDepositData The new deposit data.
-    function setDepositData(bytes memory _newDepositData) external {
-        _assertOnlyProxyAdminOwner();
-        bytes memory oldDepositData = depositData;
-        depositData = _newDepositData;
-        emit DepositDataUpdated(oldDepositData, _newDepositData);
     }
 }
