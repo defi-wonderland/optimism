23c749599811f9db49ab7d780fc1313291316cdf
0xchin <77933451+0xChin@users.noreply.github.com>
2025-10-22 17:29:02 -0300

    fix: inaccurate comment in FeeSplitter

--- packages/contracts-bedrock/src/L2/FeeSplitter.sol
+++ packages/contracts-bedrock/src/L2/FeeSplitter.sol
@@ -18,8 +18,8 @@ import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/I
 /// @custom:proxied
 /// @custom:predeploy 0x420000000000000000000000000000000000002B
 /// @title FeeSplitter
-/// @notice Withdraws funds from system FeeVault contracts, sends Optimism their revenue share, and
-///         sends the remaining funds to the fee router.
+/// @notice Withdraws funds from system FeeVault contracts and distributes them according to the
+///         configured SharesCalculator.
 contract FeeSplitter is ISemver, Initializable {
     /// @notice Thrown when the fee disbursement interval exceeds the maximum allowed.
     error FeeSplitter_ExceedsMaxFeeDisbursementTime();
