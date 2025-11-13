f23f2d3b09ee812b4854d0839c0b87bf2f764442
Ashitaka <ashitaka@defi.sucks>
2025-11-13 19:31:57 -0300

    fix: version

--- packages/contracts-bedrock/src/L1/OPContractsManager.sol
+++ packages/contracts-bedrock/src/L1/OPContractsManager.sol
@@ -2229,9 +2229,9 @@ contract OPContractsManager is ISemver {
 
     // -------- Constants and Variables --------
 
-    /// @custom:semver 5.6.1
+    /// @custom:semver 5.6.0
     function version() public pure virtual returns (string memory) {
-        return "5.6.1";
+        return "5.6.0";
     }
 
     OPContractsManagerGameTypeAdder public immutable opcmGameTypeAdder;
