e0f1f5c791f716a20c5f4f08a765f3bf147439f4
niha <205694301+0xniha@users.noreply.github.com>
2025-12-02 17:22:09 -0300

    feat: add opcmv2 flag to deployImplementations and apply opcmv2 deployment test
    * feat: add opcmv2 flag to deployImplementations and add apply opcmv2 deployment test
    
    * fix: hardcode flag to avoid import cycles & refactor createOPCMContractV2
    
    * fix: remove todo comment
    
    * fix: deploy implementations test branching
    
    * refactor: add deployOPCMStandardValidatorV2
    
    * fix: add test and script checks

--- packages/contracts-bedrock/scripts/deploy/Deploy.s.sol
+++ packages/contracts-bedrock/scripts/deploy/Deploy.s.sol
@@ -301,14 +301,20 @@ contract Deploy is Deployer {
         // Save the implementation addresses which are needed outside of this function or script.
         // When called in a fork test, this will overwrite the existing implementations.
         artifacts.save("MipsSingleton", address(dio.mipsSingleton));
-        if (DevFeatures.isDevFeatureEnabled(dio.opcm.devFeatureBitmap(), DevFeatures.OPCM_V2)) {
+        if (DevFeatures.isDevFeatureEnabled(cfg.devFeatureBitmap(), DevFeatures.OPCM_V2)) {
             artifacts.save("OPContractsManagerV2", address(dio.opcmV2));
         } else {
             artifacts.save("OPContractsManager", address(dio.opcm));
         }
         artifacts.save("DelayedWETHImpl", address(dio.delayedWETHImpl));
         artifacts.save("PreimageOracle", address(dio.preimageOracleSingleton));
+<<<<<<< HEAD
         artifacts.save("PermissionedDisputeGame", address(dio.permissionedDisputeGameV2Impl));
+=======
+        if (DevFeatures.isDevFeatureEnabled(cfg.devFeatureBitmap(), DevFeatures.DEPLOY_V2_DISPUTE_GAMES)) {
+            artifacts.save("PermissionedDisputeGame", address(dio.permissionedDisputeGameV2Impl));
+        }
+>>>>>>> 62dd7c2364 (feat: add opcmv2 flag to deployImplementations and apply opcmv2 deployment test)
 
         // Get a contract set from the implementation addresses which were just deployed.
         Types.ContractSet memory impls = ChainAssertions.dioToContractSet(dio);
@@ -336,10 +342,16 @@ contract Deploy is Deployer {
             _mips: IMIPS64(address(dio.mipsSingleton)),
             _oracle: IPreimageOracle(address(dio.preimageOracleSingleton))
         });
+        IOPContractsManager _opcm;
+        if (DevFeatures.isDevFeatureEnabled(cfg.devFeatureBitmap(), DevFeatures.OPCM_V2)) {
+            _opcm = IOPContractsManager(address(dio.opcmV2));
+        } else {
+            _opcm = IOPContractsManager(address(dio.opcm));
+        }
         ChainAssertions.checkOPContractsManager({
             _impls: impls,
             _proxies: _proxies(),
-            _opcm: IOPContractsManager(address(dio.opcm)),
+            _opcm: _opcm,
             _mips: IMIPS64(address(dio.mipsSingleton))
         });
         ChainAssertions.checkSystemConfigImpls(impls);
@@ -376,6 +388,12 @@ contract Deploy is Deployer {
         artifacts.save("AnchorStateRegistryProxy", address(deployOutput.anchorStateRegistryProxy));
         artifacts.save("OptimismPortalProxy", address(deployOutput.optimismPortalProxy));
         artifacts.save("OptimismPortal2Proxy", address(deployOutput.optimismPortalProxy));
+<<<<<<< HEAD
+=======
+        if (!DevFeatures.isDevFeatureEnabled(cfg.devFeatureBitmap(), DevFeatures.DEPLOY_V2_DISPUTE_GAMES)) {
+            artifacts.save("PermissionedDisputeGame", address(deployOutput.permissionedDisputeGame));
+        }
+>>>>>>> 62dd7c2364 (feat: add opcmv2 flag to deployImplementations and apply opcmv2 deployment test)
 
         // Check if the permissionless game implementation is already set
         IDisputeGameFactory factory = IDisputeGameFactory(artifacts.mustGetAddress("DisputeGameFactoryProxy"));
