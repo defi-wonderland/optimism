0b8feb2a42e1b21d7fc8f2dc5caa4f6982ab6b23
IamFlux <175354924+0xiamflux@users.noreply.github.com>
2025-12-10 13:13:47 -0500

    refactor: match assert pattern on OPCM v1 (#749)
    * refactor: adheres OPCM v1 revert function to assert pattern
    
    * chore: enable deny_warnings on foundry.toml
    
    * chore: pre-pr ready

--- packages/contracts-bedrock/interfaces/L1/IOPContractsManager.sol
+++ packages/contracts-bedrock/interfaces/L1/IOPContractsManager.sol
@@ -300,7 +300,7 @@ interface IOPContractsManager {
 
     error InvalidDevFeatureAccess(bytes32 devFeature);
 
-    error OPContractsManager_V1Disabled();
+    error OPContractsManager_V2Enabled();
 
     // -------- Methods --------
 
