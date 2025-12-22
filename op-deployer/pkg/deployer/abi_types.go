2c9c6d7a5a1157b5921e1a7243f93847038e4572
Flux <175354924+0xiamflux@users.noreply.github.com>
2025-12-22 13:47:12 -0500

    refactor: make abi_types use MustType

--- op-deployer/pkg/deployer/abi_types.go
+++ op-deployer/pkg/deployer/abi_types.go
@@ -1,11 +1,13 @@
 package deployer
 
-import "github.com/ethereum/go-ethereum/accounts/abi"
+import (
+	"github.com/ethereum-optimism/optimism/op-deployer/pkg/deployer/opcm"
+)
 
 // Primitive ABI types primarily used with `abi.Arguments` to pack/unpack values when calling contract methods.
 var (
-	Uint256Type, _ = abi.NewType("uint256", "", nil)
-	BytesType, _   = abi.NewType("bytes", "", nil)
-	AddressType, _ = abi.NewType("address", "", nil)
-	Bytes32Type, _ = abi.NewType("bytes32", "", nil)
+	Uint256Type = opcm.MustType("uint256")
+	BytesType   = opcm.MustType("bytes")
+	AddressType = opcm.MustType("address")
+	Bytes32Type = opcm.MustType("bytes32")
 )
