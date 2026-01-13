f36a5759ddc8ac1a6a22724e5b01f1b5ef51e693
Flux <175354924+0xiamflux@users.noreply.github.com>
2026-01-13 12:08:39 -0500

    chore: fix tests naming

--- packages/contracts-bedrock/test/opcm/InteropMigration.t.sol
+++ packages/contracts-bedrock/test/opcm/InteropMigration.t.sol
@@ -251,7 +251,7 @@ contract InteropMigrationV1_Test is Test {
         migration.run(input, output);
     }
 
-    function test_opcmWithNoCode_reverts() public {
+    function test_opcmV1_withNoCode_reverts() public {
         // Set an address with no code as OPCM
         address emptyOPCM = makeAddr("emptyOPCM");
         input.set(input.opcm.selector, emptyOPCM);
@@ -339,7 +339,7 @@ contract InteropMigrationV2_Test is Test {
         migration.run(input, output);
     }
 
-    function test_opcmWithNoCode_reverts() public {
+    function test_opcmv2_withNoCode_reverts() public {
         // Set an address with no code as OPCM
         address emptyOPCM = makeAddr("emptyOPCM");
         input.set(input.opcm.selector, emptyOPCM);
