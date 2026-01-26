cc0444f19a459e7ca87b367996206e4903a5a4e9
Flux <175354924+0xiamflux@users.noreply.github.com>
2026-01-26 15:27:05 -0500

    chore: add TODO comment for OPCM v2 support

--- op-chain-ops/interopgen/recipe.go
+++ op-chain-ops/interopgen/recipe.go
@@ -90,7 +90,8 @@ func (recipe *InteropDevRecipe) Build(addrs devkeys.Addresses) (*WorldConfig, er
 			RequiredProtocolVersion:    params.OPStackSupport,
 			RecommendedProtocolVersion: params.OPStackSupport,
 			SuperchainConfigGuardian:   superchainConfigGuardian,
-			IsOPCMv2:                   false,
+			// TODO(#18942): Update this to configurable values once op-chain-ops fully supports OPCMv2.
+			IsOPCMv2: false,
 		},
 	}
 	world := &WorldConfig{
