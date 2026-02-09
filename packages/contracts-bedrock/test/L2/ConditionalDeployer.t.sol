d0aad246ddf02ac4bf2388f30c0dcf3f044641d6
niha <205694301+0xniha@users.noreply.github.com>
2026-02-09 18:27:36 -0300

    fix: pre pr fix

--- packages/contracts-bedrock/test/L2/ConditionalDeployer.t.sol
+++ packages/contracts-bedrock/test/L2/ConditionalDeployer.t.sol
@@ -139,21 +139,12 @@ contract ConditionalDeployer_Deploy_Test is ConditionalDeployer_TestInit {
 
     /// @notice Tests that `deploy` reverts when the deployment call succeeds but no code is deployed.
     /// @dev The deployment call to the DeterministicDeploymentProxy is mocked to succeed with no code deployed.
-    function testFuzz_deploy_deploymentFailed_codeLengthZero_reverts(
-        address _caller,
-        bytes32 _salt,
-        uint256 _value
-    )
-        public
-    {
+    function testFuzz_deploy_deploymenCodeLengthZero_reverts(address _caller, bytes32 _salt, uint256 _value) public {
         bytes memory _initCode = abi.encodePacked(simpleContractCreationCode, abi.encode(0));
 
         // Mock the deployment call to the DeterministicDeploymentProxy to succeed but deploy no code
         vm.mockCall(
-            conditionalDeployer.deterministicDeploymentProxy(),
-            _value,
-            abi.encodePacked(_salt, _initCode),
-            bytes("")
+            conditionalDeployer.deterministicDeploymentProxy(), _value, abi.encodePacked(_salt, _initCode), bytes("")
         );
 
         // Deal ETH to the caller and call `deploy`
