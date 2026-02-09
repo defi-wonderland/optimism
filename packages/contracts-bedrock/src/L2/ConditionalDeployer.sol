1d53f943f1cb68c1f2c58aee026c4344a63b1420
niha <205694301+0xniha@users.noreply.github.com>
2026-02-09 14:59:27 -0300

    fix: add msg.value to conditional deployer

--- packages/contracts-bedrock/src/L2/ConditionalDeployer.sol
+++ packages/contracts-bedrock/src/L2/ConditionalDeployer.sol
@@ -31,11 +31,10 @@ contract ConditionalDeployer is ISemver {
     string public constant version = "1.0.0";
 
     /// @notice Deploys an implementation using CREATE2 if it doesn't already exist.
-    /// @param _value The amount of ETH to send with the deployment.
     /// @param _salt The salt to use for CREATE2 deployment.
     /// @param _code The initialization code for the contract.
     /// @return implementation_ The address of the deployed or existing implementation.
-    function deploy(uint256 _value, bytes32 _salt, bytes memory _code) external returns (address implementation_) {
+    function deploy(bytes32 _salt, bytes memory _code) external payable returns (address implementation_) {
         // Compute the address where the contract will be deployed using CREATE2 formula
         bytes32 codeHash = keccak256(_code);
         implementation_ = address(
@@ -48,10 +47,10 @@ contract ConditionalDeployer is ISemver {
             return implementation_;
         }
 
-        // Deploy using DeterministicDeploymentProxy (Nick's method)
+        // Deploy using Arachnid's DeterministicDeploymentProxy
         // Calldata format: salt + initcode
         (bool success, bytes memory data) =
-            DETERMINISTIC_DEPLOYMENT_PROXY.call{ value: _value }(abi.encodePacked(_salt, _code));
+            DETERMINISTIC_DEPLOYMENT_PROXY.call{ value: msg.value }(abi.encodePacked(_salt, _code));
         if (!success) {
             revert ConditionalDeployer_DeploymentFailed(data);
         }
@@ -60,8 +59,8 @@ contract ConditionalDeployer is ISemver {
         return implementation_;
     }
 
-    /// @notice Returns the address of the DeterministicDeploymentProxy (Nick's method).
-    /// @return deterministicDeploymentProxy_ The address of the DeterministicDeploymentProxy (Nick's method).
+    /// @notice Returns the address of the Arachnid's DeterministicDeploymentProxy.
+    /// @return deterministicDeploymentProxy_ The address of the Arachnid's DeterministicDeploymentProxy.
     function deterministicDeploymentProxy() external pure returns (address payable deterministicDeploymentProxy_) {
         deterministicDeploymentProxy_ = DETERMINISTIC_DEPLOYMENT_PROXY;
     }
